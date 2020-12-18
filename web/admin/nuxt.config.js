7e789d275421488dc786c7fd9cc7b254c93fb618
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-12-19 01:16:15 +0900

    feat: ディレクトリ構成の変更

--- web/admin/nuxt.config.js
+++ web/admin/nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  srcDir: 'app',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - gran-book',
