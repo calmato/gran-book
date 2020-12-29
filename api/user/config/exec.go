914b9f55c61235148b8ee37afa4bbb9448a89324
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-12-29 15:57:35 +0900

    feat: User APIの起動設定

---
+++ api/user/config/exec.go
@@ -0,0 +1,31 @@
+package config
+
+// Execute - gRPC Serverの起動
+func Execute() error {
+	env, err := loadEnvironment()
+	if err != nil {
+		return err
+	}
+
+	// gRPC Serverの設定取得
+	gs, err := newGRPCServer(env.Port, env.LogPath, env.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	// メトリクス用のHTTP Serverの起動
+	hs := newHTTPServer(env.MetricsPort)
+
+	go func() {
+		if err := hs.Serve(); err != nil {
+			panic(err)
+		}
+	}()
+
+	// gRPC Serverの起動
+	if err := gs.Serve(); err != nil {
+		panic(err)
+	}
+
+	return nil
+}
