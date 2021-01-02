54e441c8453cefddab6328ffd5790621d6034c0a
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-01-02 23:05:46 +0900

    fix: stateを使ってvalueをいじれるようにする
    validationもつけた

--- native/app/components/molecules/MailInput.tsx
+++ native/app/components/molecules/MailInput.tsx
@@ -1,16 +1,33 @@
 import { Ionicons } from '@expo/vector-icons';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { colors, Input } from 'react-native-elements';
+import { emailValidation } from '~/lib/validation';
 
 const color = colors.grey0;
 
-const MailInput = function MailInput(): ReactElement {
+interface Props {
+  onChangeText: (value: string) => void | undefined,
+  value: string | undefined,
+}
+
+const MailInput = function MailInput(props: Props): ReactElement {
+
+  const [hasError, setError] = useState<boolean>();
+
+  const onChangeText = (value: string) => {
+    !emailValidation(value) ? setError(true) : setError(false);
+    props.onChangeText(value);
+  };
+
   return (
     <Input
       leftIcon={
         <Ionicons name="md-mail" size={24} color={color} />
       }
+      onChangeText={(text) => onChangeText(text)}
+      value={props.value}
       placeholder="メールアドレス"
+      errorMessage={hasError ? 'メールアドレスを入力してください．' : undefined }
     />
   );
 };
