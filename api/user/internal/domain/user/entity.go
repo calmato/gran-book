589b4fa36d693b26a6396bf00efdd152658a0873
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-01-02 13:19:30 +0900

    feat: ER図作成に伴うUserエンティティの編集

--- api/user/internal/domain/user/entity.go
+++ api/user/internal/domain/user/entity.go
@@ -4,24 +4,24 @@ import "time"
 
 // User - Userエンティティ
 type User struct {
-	ID               string `gorm:"primaryKey;not null;<-:create"`
-	Username         string `gorm:"size:32;not null"`
-	Email            string `gorm:"size:256"`
-	Password         string `gorm:"-"`
-	Gender           int32  `gorm:"not null;default:0"`
-	Role             int32  `gorm:"not null;default:0"`
-	ThumbnailURL     string
+	ID               string    `gorm:"primaryKey;not null;<-:create"`
+	Username         string    `gorm:"size:32;not null"`
+	Gender           int32     `gorm:"not null;default:0"`
+	Email            string    `gorm:"size:256"`
+	PhoneNumber      string    `gorm:"size:16"`
+	Role             int32     `gorm:"not null;default:0"`
+	Password         string    `gorm:"-"`
+	ThumbnailURL     string    `gorm:""`
 	SelfIntroduction string    `gorm:"size:256"`
-	Lastname         string    `gorm:"size:16"`
-	Firstname        string    `gorm:"size:16"`
-	LastnameKana     string    `gorm:"size:32"`
-	FirstnameKana    string    `gorm:"size:32"`
+	LastName         string    `gorm:"size:16"`
+	FirstName        string    `gorm:"size:16"`
+	LastNameKana     string    `gorm:"size:32"`
+	FirstNameKana    string    `gorm:"size:32"`
 	PostalCode       string    `gorm:"size:16"`
 	Prefecture       string    `gorm:"size:32"`
 	City             string    `gorm:"size:32"`
 	AddressLine1     string    `gorm:"size:64"`
 	AddressLine2     string    `gorm:"size:64"`
-	PhoneNumber      string    `gorm:"size:16"`
 	InstanceID       string    `gorm:"size:256"`
 	CreatedAt        time.Time `gorm:"not null;<-:create"`
 	UpdatedAt        time.Time `gorm:"not null"`
