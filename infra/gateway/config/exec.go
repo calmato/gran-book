95e4a901ed884d5f478cb66dfe4a08c5a4204bc6
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-01-03 00:41:17 +0900

    style: lint

--- infra/gateway/config/exec.go
+++ infra/gateway/config/exec.go
@@ -2,18 +2,10 @@ package config
 
 import (
 	"context"
-	"flag"
 	"net/http"
 
 	"github.com/calmato/gran-book/infra/gateway/internal/handler"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-)
-
-var (
-	// command-line options:
-	// gRPC server endpoint
-	helloAPIEndpoint = flag.String("HELLO_API", "hello_api:8080", "gRPC server endpoint")
-	userAPIEndpoint  = flag.String("USER_API", "user_api:8080", "gRPC server endpoint")
 )
 
 // Execute - gRPC Gatewayの起動
