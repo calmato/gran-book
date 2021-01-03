e9a2b1ac49457b091d1291668d1277d3dd40dad0
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-01-04 01:47:44 +0900

    feat: Firebaseの設定

---
+++ web/admin/app/plugins/firebase.ts
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+
+const config = {
+  apiKey: process.env.firebaseApiKey,
+  authDomain: `${process.env.firebaseProjectId}.firebaseapp.com`,
+  databaseURL: `https://${process.env.firebaseProjectId}.firebaseio.com`,
+  projectId: process.env.firebaseProjectId,
+  storageBucket: `${process.env.firebaseProjectId}.appspot.com`,
+  messagingSenderId: process.env.firebaseMessagingSenderId
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+export default ({ _ }, inject) => {
+  inject('firebase', firebase)
+}
