b30e9258066538ce2c2c78d0cc78df704cd85ed7
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-01-04 00:41:17 +0900

    feat: サインイン画面のUI(仮)を作成

---
+++ web/admin/app/pages/signin.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-col cols="12" sm="8">
+    <v-img src="/logo.png" max-height="96px" contain class="mb-8" />
+
+    <v-card :rounded="32">
+      <v-card-title class="justify-center">
+        <h3>管理者向けログイン</h3>
+      </v-card-title>
+      <v-divider />
+      <v-card-text class="px-12">
+        <v-form class="py-8">
+          <v-text-field label="email" autofocus outlined />
+          <v-text-field label="password" outlined />
+          <v-btn :block="true" color="primary" class="mt-4">ログイン</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-col>
+</template>
