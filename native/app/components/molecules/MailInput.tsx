e68a9dd7618ddf07ac07d9f7d2ec07cbecde5af7
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-01-03 21:54:06 +0900

    fix: stateの管理方法を変更，キーボードタイプを追加

--- native/app/components/molecules/MailInput.tsx
+++ native/app/components/molecules/MailInput.tsx
@@ -1,33 +1,27 @@
 import { Ionicons } from '@expo/vector-icons';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import { colors, Input } from 'react-native-elements';
-import { emailValidation } from '~/lib/validation';
 
 const color = colors.grey0;
 
 interface Props {
   onChangeText: (value: string) => void | undefined,
-  value: string | undefined,
+  value: string,
+  hasError: boolean,
 }
 
 const MailInput = function MailInput(props: Props): ReactElement {
-
-  const [hasError, setError] = useState<boolean>();
-
-  const onChangeText = (value: string) => {
-    !emailValidation(value) ? setError(true) : setError(false);
-    props.onChangeText(value);
-  };
 
   return (
     <Input
       leftIcon={
         <Ionicons name="md-mail" size={24} color={color} />
       }
-      onChangeText={(text) => onChangeText(text)}
+      onChangeText={(text) => props.onChangeText(text)}
       value={props.value}
+      keyboardType="email-address"
       placeholder="メールアドレス"
-      errorMessage={hasError ? 'メールアドレスを入力してください．' : undefined }
+      errorMessage={props.hasError ? 'メールアドレスを入力してください．' : undefined }
     />
   );
 };
