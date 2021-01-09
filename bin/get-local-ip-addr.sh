4c6def7b40b9900329c53dfd9f44c36d78edb2e3
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-01-09 11:19:03 +0900

    style: スペースの追加

--- bin/get-local-ip-addr.sh
+++ bin/get-local-ip-addr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 IP=`ifconfig | grep inet | cut -d " " -f2 | grep "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | awk 'NR==2'`
-sed -i".bak" -e "/LOCAL_IP_ADDRESS=/d" .env
+sed -i ".bak" -e "/LOCAL_IP_ADDRESS=/d" .env
 echo "LOCAL_IP_ADDRESS=$IP" >> .env
