c1c03a945e94a8a02036dcc66e3f895f5532f2bd
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-01-09 22:17:09 +0900

    refactor: SignInのコンポーネントを分割

--- web/admin/app/components/templates/SignIn.vue
+++ web/admin/app/components/templates/SignIn.vue
@@ -1,5 +1,49 @@
 <template>
-  <v-row justify="center" align="center">
-    test
-  </v-row>
+  <v-container fill-height>
+    <v-layout wrap>
+      <v-row justify="center" align="center">
+        <v-col cols="12" sm="8">
+          <v-img src="/logo.png" max-height="96px" contain class="mb-8" />
+
+          <sign-in-card>
+            <sign-in-form :form="form" @click="onClickSubmitButton" />
+          </sign-in-card>
+        </v-col>
+      </v-row>
+    </v-layout>
+  </v-container>
 </template>
+
+<script lang="ts">
+import { defineComponent, reactive, SetupContext, PropType } from '@nuxtjs/composition-api'
+import { ISignInForm } from '~/types/forms'
+import SignInCard from '~/components/organisms/SignInCard.vue'
+import SignInForm from '~/components/organisms/SignInForm.vue'
+
+export default defineComponent({
+  components: {
+    SignInCard,
+    SignInForm,
+  },
+
+  props: {
+    form: {
+      type: Object as PropType<ISignInForm>,
+      required: true,
+    },
+  },
+
+  setup(props, { emit }: SetupContext) {
+    const { form } = props
+
+    const onClickSubmitButton = () => {
+      emit('submit')
+    }
+
+    return {
+      form,
+      onClickSubmitButton,
+    }
+  },
+})
+</script>
