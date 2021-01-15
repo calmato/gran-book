6b3685bc206a22411ed801a41e2222c3a3c35c3d
Atsuhide <58492130+Atsuhide@users.noreply.github.com>
2021-01-15 15:45:34 +0900

    Update index.ts
    にしくんコメントあざっした！変えます！

--- native/app/lib/validation/index.ts
+++ native/app/lib/validation/index.ts
@@ -3,7 +3,7 @@ export const emailValidation = (value: string | undefined): boolean => {
   if (value) {
     return (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/).test(value);
   } else {
-    return true;
+    return false;
   }
 };
 
