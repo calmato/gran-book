c30bbb5384498ec2324cf02eab93162e563d6b3d
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-01-21 01:22:12 +0900

    test: CheckBoxのテストを追加

---
+++ native/test/components/molecules/CheckBox.test.tsx
@@ -0,0 +1,36 @@
+import 'jest';
+import React from 'react';
+
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16.1';
+import CheckBox from '~/components/molecules/CheckBox';
+import { Text, View, ViewStyle } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { colors } from 'react-native-elements';
+
+configure({ adapter: new Adapter() });
+
+describe('<CheckBox />', () => {
+  it('has default props', () => {
+    const title = 'test';
+    const checked = true;
+    const viewStyle: ViewStyle = {
+      flexDirection: 'row',
+      alignItems: 'center',
+    };
+
+    const wrapper = shallow(<CheckBox title={title} checked={checked} onPress={() => console.log('test')} />);
+
+    const view = wrapper.find(View).get(0);
+    const icon = wrapper.find(Ionicons).get(0);
+    const text = wrapper.find(Text).get(0);
+
+    expect(view.props.style).toEqual(viewStyle);
+
+    expect(icon.props.name).toEqual('md-checkbox');
+    expect(icon.props.size).toEqual(24);
+    expect(icon.props.color).toEqual(colors.primary);
+
+    expect(text.props.children).toEqual(title);
+  });
+});
