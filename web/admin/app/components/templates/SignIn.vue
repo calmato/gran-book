bcd86512ecd8f8dfd902e72a1be1e8f792659e18
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-01-31 21:46:56 +0900

    feat: ログインエラーのアラート表示をするように

--- web/admin/app/components/templates/SignIn.vue
+++ web/admin/app/components/templates/SignIn.vue
@@ -4,7 +4,9 @@
       <v-row justify="center" align="center">
         <v-col cols="12" sm="8">
           <v-img src="/logo.png" max-height="96px" contain class="mb-8" />
-
+          <the-alert :show.sync="showAlert" type="error">
+            メールアドレス もしくは パスワード が間違っています
+          </the-alert>
           <sign-in-card>
             <sign-in-form :form="form" @click="onClickSubmitButton" />
           </sign-in-card>
@@ -15,13 +17,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, SetupContext, PropType } from '@nuxtjs/composition-api'
+import { defineComponent, computed, SetupContext, PropType } from '@nuxtjs/composition-api'
 import { ISignInForm } from '~/types/forms'
+import TheAlert from '~/components/molecules/TheAlert.vue'
 import SignInCard from '~/components/organisms/SignInCard.vue'
 import SignInForm from '~/components/organisms/SignInForm.vue'
 
 export default defineComponent({
   components: {
+    TheAlert,
     SignInCard,
     SignInForm,
   },
@@ -31,14 +35,25 @@ export default defineComponent({
       type: Object as PropType<ISignInForm>,
       required: true,
     },
+    hasError: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
   },
 
-  setup(_, { emit }: SetupContext) {
+  setup(props, { emit }: SetupContext) {
+    const showAlert = computed({
+      get: () => props.hasError,
+      set: (val: Boolean) => emit('update:hasError', val),
+    })
+
     const onClickSubmitButton = () => {
       emit('submit')
     }
 
     return {
+      showAlert,
       onClickSubmitButton,
     }
   },
