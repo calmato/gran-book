89094bc3a3e597b9e5e0a0ec83986d41bccc2ce1
atsuhide <atsuhide.i@gmail.com>
2021-02-01 12:20:36 +0900

    feat: 画面の詳細作成

--- native/app/screens/PasswordReset.tsx
+++ native/app/screens/PasswordReset.tsx
@@ -3,31 +3,57 @@ import { StyleSheet, View } from 'react-native';
 import { AuthStackParamList } from '~/types/navigation';
 import { StackNavigationProp } from '@react-navigation/stack';
 import HeaderWithBackButton from '~/components/organisms/HeaderWithBackButton';
+import MailInput from '~/components/molecules/MailInput';
+import { PasswordResetForm } from '~/types/forms';
+import { emailValidation } from '~/lib/validation';
+import { Button } from 'react-native-elements';
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  }
+});
+
+type PasswordResetProp = StackNavigationProp<AuthStackParamList, 'PasswordReset'>
+
+interface Props{
+  navigation: PasswordResetProp,
+}
+
+const PasswordReset = function PasswordReset(props: Props): ReactElement {
+  const navigation = props.navigation;
+
+  const [formData, setValue] = useState<PasswordResetForm>({
+    email: ''
   });
 
-  type PasswordResetProp = StackNavigationProp<AuthStackParamList, 'PasswordReset'>
+  const emailError: boolean = useMemo((): boolean => {
+    return !emailValidation(formData.email);
+  }, [formData.email]);
 
-  interface Props{
-      navigation: PasswordResetProp,
-  }
+  const canSubmit = useMemo((): boolean => {
+    return !emailError;
+  }, [emailError]);
 
-  const PasswordReset = function PasswordReset(props: Props): ReactElement {
-      const navigation = props.navigation;
+  return (
+    <View style={styles.container}>
+      <HeaderWithBackButton
+        title='パスワードリセット'
+        onPress={() => navigation.goBack()}
+      />
+      <MailInput
+        onChangeText={(text) => setValue({ ...formData, email: text})}
+        hasError={emailError}
+        value={formData?.email}
+      />
+      <Button
+        disabled={!canSubmit}
+        onPress={() => undefined}
+        title="メールを送信する"
+      />
+    </View>
+  );
+};
 
-      return (
-          <View style={styles.container}>
-              <HeaderWithBackButton
-                title='パスワードリセット'
-                onPress={() => navigation.goBack()}
-              />
-          </View>
-      );
-  };
-
-  export default PasswordReset;
+export default PasswordReset;
