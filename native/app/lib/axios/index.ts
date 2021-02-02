cc086df74a5cfd63f6e034cda8c9d1aff452192f
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-02-02 22:27:04 +0900

    feat: Axiosの設定

---
+++ native/app/lib/axios/index.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+const instance: AxiosInstance = axios.create({
+  baseURL: process.env.API_URL,
+})
+
+instance.interceptors.request.use((config: AxiosRequestConfig) => {
+  // TODO: set authorization token
+
+  return config
+})
+
+export default instance
