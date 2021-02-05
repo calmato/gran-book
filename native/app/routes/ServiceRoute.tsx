6a5881ca02636c6f5226ce39fd0fb72e7f6c3f25
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-02-05 23:27:02 +0900

    feat: ServiceRouterの定義を更新
    ログイン後の画面遷移は基本的にこれで行う

--- native/app/routes/ServiceRoute.tsx
+++ native/app/routes/ServiceRoute.tsx
@@ -1,18 +1,22 @@
+import React, { ReactElement } from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React, { ReactElement } from 'react';
 import { StyleSheet } from 'react-native';
-import { colors } from 'react-native-elements';
 import Home from '~/screens/Home';
 import MyPage from '~/screens/MyPage';
 import { COLOR } from '~~/constants/theme';
+import Store from '~/screens/Store';
+import Bookshelf from '~/screens/Bookshelf';
+import Sale from '~/screens/Sale';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  labelStyle: {
+    fontWeight: 'bold',
+  }
+});
 
-interface Props {}
-
-function TabBarIcon(name, focused, size) {
-  let iconName;
+function TabBarIcon(name: string, focused: boolean, size: number) {
+  let iconName: any;
 
   switch (name) {
   case 'ホーム':
@@ -21,13 +25,22 @@ function TabBarIcon(name, focused, size) {
   case 'マイページ':
     iconName = 'account';
     break;
+  case '本棚':
+    iconName= 'bookshelf';
+    break;
+  case '本を出品':
+    iconName = 'plus-circle-outline';
+    break;
+  case '本を買う':
+    iconName = 'cart';
+    break;
   default:
     iconName = 'home';
     break;
   }
 
-  const color = focused ? COLOR.MAIN : colors.grey0;
-        
+  const color = focused ? COLOR.PRIMARY : COLOR.TEXT_GRAY;
+
   return <MaterialCommunityIcons name={iconName} color={color} size={size} />;
 }
 
@@ -41,13 +54,18 @@ const ServiceRoute = function ServiceRoute(): ReactElement {
           return TabBarIcon(route.name, focused, size);
         },
       })}
+      tabBarOptions={{
+        activeTintColor: COLOR.TEXT_TITLE,
+        labelStyle: styles.labelStyle
+      }}
     >
       <Tab.Screen name="ホーム" component={Home} />
+      <Tab.Screen name="本棚" component={Bookshelf} />
+      <Tab.Screen name="本を出品" component={Sale} />
+      <Tab.Screen name="本を買う" component={Store} />
       <Tab.Screen name="マイページ" component={MyPage} />
     </Tab.Navigator>
   );
 };
-
-// ServiceRoute.defaultProps={}
 
 export default ServiceRoute;
