857ea949964e004783ab6e85fbfd777d0bd74784
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-02-07 00:59:52 +0900

    test: hooksのテストの書き方がわからないので一旦放置

--- native/test/components/molecules/PasswordInput.test.tsx
+++ native/test/components/molecules/PasswordInput.test.tsx
@@ -3,11 +3,18 @@ import React from 'react';
 
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16.1';
+import PasswordInput from '~/components/molecules/PasswordInput';
 
 configure({ adapter: new Adapter() });
 
 describe('<PasswordInput />', () => {
   it('has default props', () => {
-    const wrapper = shallow();
+    const placeholder = 'test';
+    const value = 'test';
+    const wrapper = shallow(<PasswordInput
+      placeholder={placeholder}
+      value={value}
+      onChangeText={() => console.log('test')}
+    />);
   });
 });
