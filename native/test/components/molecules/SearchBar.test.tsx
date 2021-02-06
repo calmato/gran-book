aff6b1c19cd4cad2d1ae62ddf22b1c21b079595d
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-02-06 23:30:56 +0900

    feat: 生成されたテストコードをコミットする

---
+++ native/test/components/molecules/SearchBar.test.tsx
@@ -0,0 +1,13 @@
+import 'jest';
+import React from 'react';
+
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16.1';
+
+configure({ adapter: new Adapter() });
+
+describe('<SearchBar />', () => {
+  it('has default props', () => {
+    const wrapper = shallow();
+  });
+});
