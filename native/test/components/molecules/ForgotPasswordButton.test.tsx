c71df2221b091dc1fd81abad82fce3139e4e95a2
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-02-07 00:57:21 +0900

    test: ForgotPasswordButtonのテストファイルの雛形の作成
    todo: いなちゃんが書いてくれるはず…

--- native/test/components/molecules/ForgotPasswordButton.test.tsx
+++ native/test/components/molecules/ForgotPasswordButton.test.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16.1';
+import ForgotPasswordButton from '~/components/molecules/ForgotPasswordButton';
 
 configure({ adapter: new Adapter() });
 
 describe('<ForgotPasswordButton />', () => {
   it('has default props', () => {
-    const wrapper = shallow();
+    const wrapper = shallow(<ForgotPasswordButton
+      onPress={()=>console.log('test')}
+    />);
   });
 });
