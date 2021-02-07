0f39f35d8e4ef0a3cd84a3fe447e36d69987a967
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-02-07 17:34:32 +0900

    refactor: コードのリファクタ

--- api/gateway/admin/src/plugins/grpc-status.ts
+++ api/gateway/admin/src/plugins/grpc-status.ts
@@ -1,30 +1,25 @@
 import { IErrorOutput } from '~/types/output'
 import { IErrorResponse } from '~/types/response'
 
-// TODO: unionに置き換え
-function convertStatusGrpcToHttp(status: number): number {
-  switch(status) {
-    case 0:
-      return 200
-    case 2:
-      return 500
-    case 3:
-      return 400
-    case 5:
-      return 404
-    case 6:
-      return 409
-    case 7:
-      return 403
-    case 13:
-      return 500
-    case 15:
-      return 503
-    case 16:
-      return 401
-    default:
-      return 500
+const statusConverter: { [key: number]: number } = {
+  0: 200, // OK -> OK
+  2: 500, // UNKNOWN -> Internal Server Error
+  3: 400, // INVALID_ARGUMENT -> Bad Request
+  5: 404, // NOT_FOUND -> Not Found
+  6: 409, // ALREADY_EXISTS -> Conflict
+  7: 403, // PREMISSION_DENIED -> Forbidden
+  13: 500, // INTERNAL -> Internal Server Error
+  14: 503, // UNAVAILABLE -> Service Unavailable
+  16: 401, // UNAUTHENTICATED -> Unauthorized
+}
+
+function convertStatusGrpcToHttp(grpcStatus: number): number {
+  const httpStatus = statusConverter[grpcStatus]
+  if (httpStatus) {
+    return httpStatus
   }
+
+  return 500
 }
 
 export function getHttpError(output: IErrorOutput): IErrorResponse {
