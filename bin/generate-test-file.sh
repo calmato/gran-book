b77bc65d3f30d8112410bc019d5a59a906e48a4d
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-02-07 23:48:14 +0900

    chore: enzyme-adapter-react-16.1からenzyme-adapter-react-16に変更

--- bin/generate-test-file.sh
+++ bin/generate-test-file.sh
@@ -8,7 +8,7 @@ import 'jest';
 import React from 'react';
 
 import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16.1';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
