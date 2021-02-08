f4500c9b35681658bf1dda6cb4adce206c4a7c21
atsuhide <atsuhide.i@gmail.com>
2021-02-08 14:49:50 +0900

    feat: 本の画像とタイトルと著者をコンポーネントにしました

--- native/app/screens/BookReadRegister.tsx
+++ native/app/screens/BookReadRegister.tsx
@@ -1,8 +1,18 @@
 import React, { ReactElement } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import BookNameAuthorRegister from '~/components/organisms/BookNameAuthorRegister';
 import HeaderWithBackButton from '~/components/organisms/HeaderWithBackButton';
+import { COLOR } from '~~/constants/theme';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+});
+
+const bookInfo = {
+  title: '何者',
+  image_url: 'https://thechara.xsrv.jp/wp-content/uploads/2020/06/200622%E3%80%90NARUTO%E3%80%91KV_02.jpg',
+  author: '稲富',
+};
+
 interface Props {}
 const BookReadRegister = function BookReadRegister(props: Props): ReactElement {
   return(
@@ -11,6 +21,11 @@ const BookReadRegister = function BookReadRegister(props: Props): ReactElement {
         title='読んだ本登録'
         onPress={() => undefined}
       />
+    <BookNameAuthorRegister 
+      title={bookInfo.title}
+      image_url={bookInfo.image_url}
+      author={bookInfo.author}
+    />
     </View>
   );
 };
