2df0f4210989d6a1d49a2d55e09f6e4da37e7e9d
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-02-13 03:06:32 +0900

    style: lint

--- api/gateway/admin/src/routes/v1/auth.ts
+++ api/gateway/admin/src/routes/v1/auth.ts
@@ -8,69 +8,92 @@ import { GrpcError } from '~/types/exception'
 
 const router = express.Router()
 
-router.get('/', async (req: Request , res: Response<IAuthResponse>, next: NextFunction): Promise<void> => {
-  await getAuth(req)
-    .then((output: IAuthOutput) => {
-      const response: IAuthResponse = setAuthResponse(output)
-      res.status(200).json(response)
-    })
-    .catch((err: GrpcError) => next(err))
-})
-
-router.post('/', async (req: Request<ICreateAuthRequest>, res: Response<IAuthResponse>, next: NextFunction): Promise<void> => {
-  const { username, email, password, passwordConfirmation } = req.body as ICreateAuthRequest
-
-  const input: ICreateAuthInput = {
-    username: username,
-    email: email,
-    password: password,
-    passwordConfirmation: passwordConfirmation,
+router.get(
+  '/',
+  async (req: Request, res: Response<IAuthResponse>, next: NextFunction): Promise<void> => {
+    await getAuth(req)
+      .then((output: IAuthOutput) => {
+        const response: IAuthResponse = setAuthResponse(output)
+        res.status(200).json(response)
+      })
+      .catch((err: GrpcError) => next(err))
   }
+)
 
-  await createAuth(req, input)
-    .then((output: IAuthOutput) => {
-      const response: IAuthResponse = setAuthResponse(output)
-      res.status(200).json(response)
-    })
-    .catch((err: GrpcError) => next(err))
-})
+router.post(
+  '/',
+  async (req: Request<ICreateAuthRequest>, res: Response<IAuthResponse>, next: NextFunction): Promise<void> => {
+    const { username, email, password, passwordConfirmation } = req.body as ICreateAuthRequest
 
-router.patch('/', async (req: Request<IUpdateAuthRequest>, res: Response<IAuthResponse>, next: NextFunction): Promise<void> => {
-  const { username, gender, thumbnail, selfIntroduction, lastName, firstName, lastNameKana, firstNameKana, phoneNumber } = req.body as IUpdateAuthRequest
+    const input: ICreateAuthInput = {
+      username: username,
+      email: email,
+      password: password,
+      passwordConfirmation: passwordConfirmation,
+    }
 
-  await getAuth(req)
-    .then((): Promise<IAuthOutput> => {
-      const input: IUpdateAuthProfileInput = {
-        username: username,
-        gender: gender,
-        thumbnail: thumbnail,
-        selfIntroduction: selfIntroduction,
-      }
+    await createAuth(req, input)
+      .then((output: IAuthOutput) => {
+        const response: IAuthResponse = setAuthResponse(output)
+        res.status(200).json(response)
+      })
+      .catch((err: GrpcError) => next(err))
+  }
+)
 
-      return updateAuthProfile(req, input)
-    })
-    .then((output: IAuthOutput): Promise<IAuthOutput> => {
-      const input: IUpdateAuthAddressInput = {
-        lastName: lastName,
-        firstName: firstName,
-        lastNameKana: lastNameKana,
-        firstNameKana: firstNameKana,
-        phoneNumber: phoneNumber,
-        postalCode: output.postalCode,
-        prefecture: output.prefecture,
-        city: output.city,
-        addressLine1: output.addressLine1,
-        addressLine2: output.addressLine2,
-      }
+router.patch(
+  '/',
+  async (req: Request<IUpdateAuthRequest>, res: Response<IAuthResponse>, next: NextFunction): Promise<void> => {
+    const {
+      username,
+      gender,
+      thumbnail,
+      selfIntroduction,
+      lastName,
+      firstName,
+      lastNameKana,
+      firstNameKana,
+      phoneNumber,
+    } = req.body as IUpdateAuthRequest
 
-      return updateAuthAddress(req, input)
-    })
-    .then((output: IAuthOutput) => {
-      const response: IAuthResponse = setAuthResponse(output)
-      res.status(200).json(response)
-    })
-    .catch((err: GrpcError) => next(err))
-})
+    await getAuth(req)
+      .then(
+        (): Promise<IAuthOutput> => {
+          const input: IUpdateAuthProfileInput = {
+            username: username,
+            gender: gender,
+            thumbnail: thumbnail,
+            selfIntroduction: selfIntroduction,
+          }
+
+          return updateAuthProfile(req, input)
+        }
+      )
+      .then(
+        (output: IAuthOutput): Promise<IAuthOutput> => {
+          const input: IUpdateAuthAddressInput = {
+            lastName: lastName,
+            firstName: firstName,
+            lastNameKana: lastNameKana,
+            firstNameKana: firstNameKana,
+            phoneNumber: phoneNumber,
+            postalCode: output.postalCode,
+            prefecture: output.prefecture,
+            city: output.city,
+            addressLine1: output.addressLine1,
+            addressLine2: output.addressLine2,
+          }
+
+          return updateAuthAddress(req, input)
+        }
+      )
+      .then((output: IAuthOutput) => {
+        const response: IAuthResponse = setAuthResponse(output)
+        res.status(200).json(response)
+      })
+      .catch((err: GrpcError) => next(err))
+  }
+)
 
 export default router
 
