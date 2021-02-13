f0d2cfaf96b7d9f92d649666b8a1d83b402ad0ce
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-02-13 18:50:34 +0900

    feat: リクエストの型定義を追加

--- api/gateway/native/src/types/request/auth.ts
+++ api/gateway/native/src/types/request/auth.ts
@@ -4,3 +4,23 @@ export interface ICreateAuthRequest {
   password: string
   passwordConfirmation: string
 }
+
+export interface IUpdateAuthProfileRequest {
+  username: string
+  gender: number
+  thumbnail: string
+  selfIntroduction: string
+}
+
+export interface IUpdateAuthAddressRequest {
+  lastName: string
+  firstName: string
+  lastNameKana: string
+  firstNameKana: string
+  phoneNumber: string
+  postalCode: string
+  prefecture: string
+  city: string
+  addressLine1: string
+  addressLine2: string
+}
