48d8b7a82fe5a007d78c804dc60ecf8f089f7d01
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-02-15 12:43:35 +0900

    test: Commonストアのテストを作成

--- web/admin/spec/helpers/store-helper.ts
+++ web/admin/spec/helpers/store-helper.ts
@@ -2,6 +2,7 @@ import Vue, { VueConstructor } from 'vue'
 import Vuex from 'vuex'
 import { createLocalVue } from '@vue/test-utils'
 import AuthStore from '~/store/auth'
+import CommonStore from '~/store/common'
 import response from '~~/spec/helpers/response'
 
 const localVue: VueConstructor<Vue> = createLocalVue()
@@ -10,6 +11,7 @@ localVue.use(Vuex)
 const store = new Vuex.Store({
   modules: {
     auth: AuthStore,
+    common: CommonStore,
   },
 })
 
