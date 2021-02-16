3acb585823047780f245f42d54c7bf958de4e08c
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-02-16 09:43:06 +0900

    feat: [WIP] プロフィール変更用の型定義

--- web/admin/app/types/forms/auth.ts
+++ web/admin/app/types/forms/auth.ts
@@ -18,6 +18,11 @@ export interface IAuthEditPasswordForm {
   options: IAuthEditPasswordOptions
 }
 
+export interface IAuthEditProfileForm {
+  params: IAuthEditProfileParams
+  options: IAuthEditProfileOptions
+}
+
 // ---------------------------
 // interface - params
 // ---------------------------
@@ -30,6 +35,17 @@ export interface IAuthEditPasswordParams {
   passwordConfirmation: string
 }
 
+export interface IAuthEditProfileParams {
+  username: string
+  thumbnail: string
+  selfIntroduction: string
+  lastName: string
+  firstName: string
+  lastNameKana: string
+  firstNameKana: string
+  phoneNumber: string
+}
+
 // ---------------------------
 // interface - options
 // ---------------------------
@@ -40,6 +56,17 @@ export interface IAuthEditEmailOptions {
 export interface IAuthEditPasswordOptions {
   password: ITextField
   passwordConfirmation: ITextField
+}
+
+export interface IAuthEditProfileOptions {
+  username: ITextField
+  thumbnail: ITextField // TODO: FileInput用コンポーネント作成後変更
+  selfIntroduction: ITextField // TODO: TextArea用コンポーネント作成後変更
+  lastName: ITextField
+  firstName: ITextField
+  lastNameKana: ITextField
+  firstNameKana: ITextField
+  phoneNumber: ITextField
 }
 
 // ---------------------------
@@ -72,4 +99,39 @@ export const AuthEditPasswordOptions: IAuthEditPasswordOptions = {
       confirmed: 'パスワード',
     },
   } as ITextField,
+}
+
+export const AuthEditProfileOptions: IAuthEditProfileOptions = {
+  username: {
+    label: '表示名',
+    rules: {} as ITextField,
+  },
+  thumbnail: {
+    label: 'サムネイル',
+    rules: {} as ITextField,
+  },
+  selfIntroduction: {
+    label: '自己紹介',
+    rules: {} as ITextField,
+  },
+  lastName: {
+    label: '姓',
+    rules: {} as ITextField,
+  },
+  firstName: {
+    label: '名',
+    rules: {} as ITextField,
+  },
+  lastNameKana: {
+    label: '姓 (かな)',
+    rules: {} as ITextField,
+  },
+  firstNameKana: {
+    label: '名 (かな)',
+    rules: {} as ITextField,
+  },
+  phoneNumber: {
+    label: '電話番号',
+    rules: {} as ITextField,
+  },
 }
--- web/admin/app/types/requests/auth.ts
+++ web/admin/app/types/requests/auth.ts
@@ -6,3 +6,14 @@ export interface IAuthUpdatePasswordRequest {
   password: string
   passwordConfirmation: string
 }
+
+export interface IAuthUpdateProfileRequest {
+  username: string
+  thumbnail: string
+  selfIntroduction: string
+  lastName: string
+  firstName: string
+  lastNameKana: string
+  firstNameKana: string
+  phoneNumber: string
+}
