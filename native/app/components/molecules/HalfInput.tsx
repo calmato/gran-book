487a4b35aa5291e5a10c06cc05fc993021b3e1be
濵田広大 <hamany0326@gmail.com>
2021-02-18 00:04:48 +0900

    fix: 半分のサイズの入力フォームのmoleculesを修正

--- native/app/components/molecules/HalfInput.tsx
+++ native/app/components/molecules/HalfInput.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
-import { TextInput } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { COLOR } from '~~/constants/theme';
 
 interface Props{
   placeholder: string,
@@ -7,15 +8,26 @@ interface Props{
   onChangeText: (value: string) => void | undefined,
 }
 
+const styles = StyleSheet.create({
+  halfInput: {
+    width: '50%',
+    padding: 15,
+    marginRight: 5,
+    backgroundColor: COLOR.BACKGROUND_WHITE,
+  },
+});
+
 const HalfInput = function HalfInput(props: Props): ReactElement {
 
   return (
-    <TextInput
-      onChangeText={(text) => props.onChangeText(text)}
-      value={props.value}
-      placeholder={props.placeholder}
-      maxLength={16}
-    />
+    <View style={styles.halfInput}>
+      <TextInput
+        onChangeText={(text) => props.onChangeText(text)}
+        value={props.value}
+        placeholder={props.placeholder}
+        maxLength={16}
+      />
+    </View>
   );
 };
 
