bb5bb06dafbfaf983660c777e51c1e5de561845e
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-02-20 13:54:22 +0900

    feat: 管理者情報更新APIの実装

--- api/gateway/admin/src/routes/v1/admin.ts
+++ api/gateway/admin/src/routes/v1/admin.ts
@@ -1,16 +1,26 @@
 import express, { Request, Response, NextFunction } from 'express'
-import { createAdmin } from '~/api/admin'
+import { createAdmin, updateAdminPassword, updateAdminProfile, updateAdminRole } from '~/api/admin'
 import { GrpcError } from '~/types/exception'
-import { ICreateAdminInput } from '~/types/input'
+import {
+  ICreateAdminInput,
+  IUpdateAdminPasswordInput,
+  IUpdateAdminProfileInput,
+  IUpdateAdminRoleInput,
+} from '~/types/input'
 import { IAdminOutput } from '~/types/output'
-import { ICreateAdminRequest } from '~/types/request'
+import {
+  ICreateAdminRequest,
+  IUpdateAdminPasswordRequest,
+  IUpdateAdminProfileRequest,
+  IUpdateAdminRoleRequest,
+} from '~/types/request'
 import { IAdminResponse } from '~/types/response'
 
 const router = express.Router()
 
 router.post(
   '/',
-  async (req: Request, res: Response<IAdminResponse>, next: NextFunction): Promise<IAdminResponse> => {
+  async (req: Request, res: Response<IAdminResponse>, next: NextFunction): Promise<void> => {
     const {
       username,
       email,
@@ -36,6 +46,72 @@ router.post(
     }
 
     await createAdmin(req, input)
+      .then((output: IAdminOutput) => {
+        const response: IAdminResponse = setAdminResponse(output)
+        res.status(200).json(response)
+      })
+      .catch((err: GrpcError) => next(err))
+  }
+)
+
+router.patch(
+  '/:userId/role',
+  async (req: Request, res: Response<IAdminResponse>, next: NextFunction): Promise<void> => {
+    const { userId } = req.params
+    const { role } = req.body as IUpdateAdminRoleRequest
+
+    const input: IUpdateAdminRoleInput = {
+      id: userId,
+      role,
+    }
+
+    await updateAdminRole(req, input)
+      .then((output: IAdminOutput) => {
+        const response: IAdminResponse = setAdminResponse(output)
+        res.status(200).json(response)
+      })
+      .catch((err: GrpcError) => next(err))
+  }
+)
+
+router.patch(
+  '/:userId/password',
+  async (req: Request, res: Response<IAdminResponse>, next: NextFunction): Promise<void> => {
+    const { userId } = req.params
+    const { password, passwordConfirmation } = req.body as IUpdateAdminPasswordRequest
+
+    const input: IUpdateAdminPasswordInput = {
+      id: userId,
+      password,
+      passwordConfirmation,
+    }
+
+    await updateAdminPassword(req, input)
+      .then((output: IAdminOutput) => {
+        const response: IAdminResponse = setAdminResponse(output)
+        res.status(200).json(response)
+      })
+      .catch((err: GrpcError) => next(err))
+  }
+)
+
+router.patch(
+  '/:userId/profile',
+  async (req: Request, res: Response<IAdminResponse>, next: NextFunction): Promise<void> => {
+    const { userId } = req.params
+    const { username, email, lastName, lastNameKana, firstName, firstNameKana } = req.body as IUpdateAdminProfileRequest
+
+    const input: IUpdateAdminProfileInput = {
+      id: userId,
+      username,
+      email,
+      lastName,
+      firstName,
+      lastNameKana,
+      firstNameKana,
+    }
+
+    await updateAdminProfile(req, input)
       .then((output: IAdminOutput) => {
         const response: IAdminResponse = setAdminResponse(output)
         res.status(200).json(response)
