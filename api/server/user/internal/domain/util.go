13420bc76aa2859934c620d7bd8d55cde680dc97
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-02-23 11:46:49 +0900

    refactor: 今使ってないもの消す

--- api/server/user/internal/domain/util.go
+++ api/server/user/internal/domain/util.go
@@ -7,22 +7,6 @@ type ListQuery struct {
 	Order  *QueryOrder
 }
 
-// SearchQuery - 検索時のクエリ用構造体
-type SearchQuery struct {
-	Limit         int64
-	Offset        int64
-	Order         *QueryOrder
-	Fields        []*QueryField
-	FieldOperator string // and,or
-}
-
-// QueryField - 詳細検索用の構造体
-type QueryField struct {
-	Field    string
-	Operator string // ==,!=,<,<=,>,>=,in,like
-	Value    interface{}
-}
-
 // QueryOrder - ソート用の構造体
 type QueryOrder struct {
 	By        string
