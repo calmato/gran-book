d4aa4e7f3223307dd4ae7a4149ddb4d78d7ea4c8
atsuhide <atsuhide.i@gmail.com>
2021-02-24 22:43:18 +0900

    feat: ニックネームのコンポーネントの作成

---
+++ native/app/components/organisms/ChangeNickname.tsx
@@ -0,0 +1,46 @@
+import React, { ReactElement } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { COLOR } from '~~/constants/theme';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    padding: 20,
+    flexDirection: 'row',
+    backgroundColor: COLOR.BACKGROUND_WHITE,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  　text: {
+    color: COLOR.TEXT_DEFAULT,
+    fontSize: 16,
+    flex: 1,
+  },
+  input: {
+    flex: 2,
+    fontSize: 16,
+  },
+});
+
+interface Props {
+  defaultValue: string,
+  handelOnChangeText: () => void,
+}
+
+const ChangeNickname = function ChangeNickname(props: Props): ReactElement {
+return (
+  <View style={styles.container}>
+    <Text style={styles.text}>ニックネーム</Text>
+    <TextInput 
+    style={styles.input} 
+    maxLength={32} 
+    textAlign='right' 
+    multiline={true} 
+    value={props.defaultValue} 
+    onChangeText={props.handelOnChangeText}/>
+  </View>
+);
+}
+
+export default ChangeNickname
