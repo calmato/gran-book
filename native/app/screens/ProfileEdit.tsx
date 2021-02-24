6282296346b6b241e313500ecee9db0e35cf7e40
atsuhide <atsuhide.i@gmail.com>
2021-02-24 21:44:45 +0900

    feat: ヘッダーとアイコン変更コンポーネントの配置

--- native/app/screens/ProfileEdit.tsx
+++ native/app/screens/ProfileEdit.tsx
@@ -1,15 +1,35 @@
 import React, { ReactElement } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import ChangeIconGroup from '~/components/organisms/ChangeIconGroup';
+import HeaderWithBackButton from '~/components/organisms/HeaderWithBackButton';
 
 const styles = StyleSheet.create({});
 
 interface Props {}
 
+const userInfo = 
+{
+  name: 'hamachans',
+  avatarUrl: 'https://pbs.twimg.com/profile_images/1312909954148253696/Utr-sa_Y_400x400.jpg',
+  rating: 2.4,
+  reviewNum: 20,
+  saleNum: 3,
+  followerNum: 20,
+  followNum: 5,
+  bio: 'よろしくお願いします。',
+};
+
 const ProfileEdit = function ProfileEdit(props: Props): ReactElement {
   return (
   <View>
-    <ChangeIconGroup/>
+    <HeaderWithBackButton 
+      title='プロフィール編集'
+      onPress={()=>undefined}
+    />
+    <ChangeIconGroup
+      avatarUrl={userInfo.avatarUrl}
+      handleOnPressed={()=>undefined}
+    />
   </View>
   );
 }
