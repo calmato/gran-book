b4b582664f119d8679f7eda366eef40530ba908b
濵田広大 <hamany0326@gmail.com>
2021-02-24 15:37:29 +0900

    refacotor: 細かい修正 part2

--- native/app/screens/AccountEdit.tsx
+++ native/app/screens/AccountEdit.tsx
@@ -1,7 +1,7 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '~/types/navigation';
 import React, { ReactElement, useState, useMemo } from 'react';
-import { StyleSheet, View, Text, ScrollView, SafeAreaView, TextInput, Button } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, SafeAreaView, TextInput, Button, KeyboardAvoidingView } from 'react-native';
 import { COLOR } from '~~/constants/theme';
 import HeaderWithBackButton from '~/components/organisms/HeaderWithBackButton';
 import { AccountEditForm } from '~/types/forms';
@@ -14,6 +14,7 @@ const maxNameLength7 = 7;
 const maxNameLength16 = 16;
 const maxNameLength32 = 32;
 const maxNameLength64 = 64;
+
 const styles = StyleSheet.create(
   {
     subtilte: {
@@ -43,7 +44,12 @@ const styles = StyleSheet.create(
       borderRadius: 10,
       backgroundColor: COLOR.PRIMARY,
     },
-
+    saveButton: {
+      width: '75%',
+      alignSelf: 'center',
+      marginTop: 20,
+      backgroundColor: COLOR.PRIMARY,
+    }
   });
 
 type AccountEditProp = StackNavigationProp<RootStackParamList, 'AccountEdit'>;
@@ -83,8 +89,17 @@ const AccountEdit = function AccountEdit(props: Props): ReactElement {
   }, [formData.postalCode]);
 
   const handleAccountEditSubmit = React.useCallback(async () => {
-    await accountEditResiter(
-      formData.postalCode
+    await accountEdit(
+      formData.firstName,
+      formData.lastName,
+      formData.firstNameKana,
+      formData.lastNameKana,
+      formData.phoneNumber,
+      formData.postalCode,
+      formData.prefecture,
+      formData.city,
+      formData.addressLine1,
+      formData.addressLine2
     );
     // .then(() => {
     // })
@@ -99,100 +114,96 @@ const AccountEdit = function AccountEdit(props: Props): ReactElement {
         title='発送元・お届け先住所'
         onPress={() => navigation.goBack()}
       />
-      <SafeAreaView>
-        <ScrollView>
-          <Text style={styles.subtilte}>
+      <Text style={styles.subtilte}>
             名前
-          </Text>
-          <View style={styles.halfInputRow}>
-            <HalfTextInput
-              onChangeText={(text) => setValue({...formData, firstName: text})}
-              value={formData.firstName}
-              placeholder='田中'
-              length={maxNameLength16}
-            />
-            <HalfTextInput
-              onChangeText={(text) => setValue({...formData, lastName: text})}
-              value={formData.lastName}
-              placeholder='太郎'
-              length={maxNameLength16}
-            />
-          </View>
-          <Text style={styles.subtilte}>
+      </Text>
+      <View style={styles.halfInputRow}>
+        <HalfTextInput
+          onChangeText={(text) => setValue({...formData, firstName: text})}
+          value={formData.firstName}
+          placeholder='田中'
+          length={maxNameLength16}
+        />
+        <HalfTextInput
+          onChangeText={(text) => setValue({...formData, lastName: text})}
+          value={formData.lastName}
+          placeholder='太郎'
+          length={maxNameLength16}
+        />
+      </View>
+      <Text style={styles.subtilte}>
             名前(かな)
-          </Text>
-          <View style={styles.halfInputRow}>
-            <HalfTextInput
-              onChangeText={(text) => setValue({...formData, firstNameKana: text})}
-              value={formData.firstNameKana}
-              placeholder='たなか'
-              length={maxNameLength16}
-            />
-            <HalfTextInput
-              onChangeText={(text) => setValue({...formData, lastNameKana: text})}
-              value={formData.lastNameKana}
-              placeholder='たろう'
-              length={maxNameLength16}
-            />
-          </View>
-          <Text style={styles.subtilte}>
+      </Text>
+      <View style={styles.halfInputRow}>
+        <HalfTextInput
+          onChangeText={(text) => setValue({...formData, firstNameKana: text})}
+          value={formData.firstNameKana}
+          placeholder='たなか'
+          length={maxNameLength16}
+        />
+        <HalfTextInput
+          onChangeText={(text) => setValue({...formData, lastNameKana: text})}
+          value={formData.lastNameKana}
+          placeholder='たろう'
+          length={maxNameLength16}
+        />
+      </View>
+      <Text style={styles.subtilte}>
             電話番号
-          </Text>
-          <NumberTextInput
-            onChangeText={(text) => setValue({...formData, phoneNumber: text})}
-            value={formData.phoneNumber}
-            placeholder=''
-            length={maxNameLength16}
-          />
-          <Text style={styles.subtilte}>
+      </Text>
+      <NumberTextInput
+        onChangeText={(text) => setValue({...formData, phoneNumber: text})}
+        value={formData.phoneNumber}
+        placeholder=''
+        length={maxNameLength16}
+      />
+      <Text style={styles.subtilte}>
             住所
-          </Text>
-          <View style={styles.postalArea}>
-            <Text>〒 </Text>
-            <TextInput
-              onChangeText={(text) => setValue({...formData, postalCode: text})}
-              value={formData.postalCode}
-              maxLength={maxNameLength7}
-              keyboardType='number-pad'
-              style={{flex:3, alignSelf: 'stretch'}}
-            >
-            </TextInput>
-            <View style={styles.searchButton}>
-              <Button onPress={handlePostaSubmit}
-                title='検索'
-                color={COLOR.TEXT_TITLE}
-              />
-            </View>
-          </View>
-          <View style={styles.prefectureArea}>
-            <PrefecturePicker />
-          </View>
-          <FullTextInput
-            onChangeText={(text) => setValue({...formData, city: text})}
-            value={formData.city}
-            placeholder='市区町村'
-            length={maxNameLength32}
-          />
-          <FullTextInput
-            onChangeText={(text) => setValue({...formData, addressLine1: text})}
-            value={formData.addressLine1}
-            placeholder='地名・番地'
-            length={maxNameLength64}
-          />
-          <FullTextInput
-            onChangeText={(text) => setValue({...formData, addressLine2: text})}
-            value={formData.addressLine2}
-            placeholder='マンション・ビル名 部屋番号'
-            length={maxNameLength64}
-          />
-          <View>
-          <Button onPress={handleAccountEditSubmit}
-            title='保存する'
+      </Text>
+      <View style={styles.postalArea}>
+        <Text>〒 </Text>
+        <TextInput
+          onChangeText={(text) => setValue({...formData, postalCode: text})}
+          value={formData.postalCode}
+          maxLength={maxNameLength7}
+          keyboardType='number-pad'
+          style={{flex:3, alignSelf: 'stretch'}}
+        >
+        </TextInput>
+        <View style={styles.searchButton}>
+          <Button onPress={handlePostaSubmit}
+            title='検索'
             color={COLOR.TEXT_TITLE}
           />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+        </View>
+      </View>
+      <View style={styles.prefectureArea}>
+        <PrefecturePicker />
+      </View>
+      <FullTextInput
+        onChangeText={(text) => setValue({...formData, city: text})}
+        value={formData.city}
+        placeholder='市区町村'
+        length={maxNameLength32}
+      />
+      <FullTextInput
+        onChangeText={(text) => setValue({...formData, addressLine1: text})}
+        value={formData.addressLine1}
+        placeholder='地名・番地'
+        length={maxNameLength64}
+      />
+      <FullTextInput
+        onChangeText={(text) => setValue({...formData, addressLine2: text})}
+        value={formData.addressLine2}
+        placeholder='マンション・ビル名 部屋番号'
+        length={maxNameLength64}
+      />
+      <View style={styles.saveButton}>
+        <Button onPress={handleAccountEditSubmit}
+          title='保存する'
+          color={COLOR.TEXT_TITLE}
+        />
+      </View>
     </View>
   );
 };
