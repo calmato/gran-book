4db1afdd88de4b01a1fcd34fbc4e8b09b384e371
atsuhide <atsuhide.i@gmail.com>
2021-02-24 14:36:53 +0900

    feat: 画面の基礎の作成

---
+++ native/app/screens/OtherProfile.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import { View } from 'react-native';
+
+const OtherProfile = function OtherProfile(): ReactElement {
+  return (
+    <View >
+      
+    </View>
+  );
+};
+
+export default OtherProfile;
