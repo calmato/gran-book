3feb1928fd3054db2657d4478638ec4d0b32741c
atsuhide <atsuhide.i@gmail.com>
2021-02-26 14:47:13 +0900

    feat: ヘッダーとメール登録に関する注意を記すTextを実装

--- native/app/screens/EmailEdit.tsx
+++ native/app/screens/EmailEdit.tsx
@@ -1,10 +1,22 @@
 import React, { ReactElement } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import HeaderWithBackButton from '~/components/organisms/HeaderWithBackButton';
+import { COLOR } from '~~/constants/theme';
 
 const styles = StyleSheet.create({
+  container:{
+    alignItems: 'center',
+  },
   textCard: {
-
+    width: '90%',
+    fontSize: 16,
+    color: COLOR.TEXT_DEFAULT,
+    backgroundColor: COLOR.BACKGROUND_YELLOW,
+    borderColor: COLOR.PRIMARY,
+    borderWidth: 2.5,
+    marginTop:10,
+    paddingStart: 30,
+    paddingVertical: 10,
   },
 });
 
@@ -12,13 +24,16 @@ interface Props {}
 
 const EmailEdit = function EmailEdit
 (props: Props): ReactElement {
+  const statusDefault = 'メールアドレス未登録';
 return (
-  <View>
-    <HeaderWithBackButton title='メールアドレスの変更' onPress={()=>undefined}/>
-    <Text style={styles.textCard}>新しいメールアドレスを入力してください。確認メールが送信されます。</Text>
+  <View style={styles.container}>
+    <HeaderWithBackButton 
+    title='メールアドレスの変更' 
+    onPress={()=>undefined}
+    />
+    <Text style={styles.textCard}>新しいメールアドレスを入力してください。{'\n'}確認メールが送信されます。</Text>
   </View>
 );
 }
 
 export default EmailEdit
-
