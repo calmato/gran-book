01d61f722be7edd85dbc0b89df461f483b567312
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-02-27 23:46:45 +0900

    feat: storageの中身のログイン情報でコンテキストを変更

--- native/app/routes/index.tsx
+++ native/app/routes/index.tsx
@@ -2,11 +2,15 @@ import React, { ReactElement } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as UiContext from '~/lib/context/ui';
+import { UiContext as Context } from '~/lib/context';
 import Onboarding from '~/screens/Onboarding';
 import AuthRoute from '~/routes/AuthRoute';
 import ServiceRoute from '~/routes/ServiceRoute';
 import { RootStackParamList } from '~/types/navigation';
-
+import { retrieve } from '~/lib/local-storage/auth-storage';
+import { Status } from '~/lib/context/ui';
+import { useState } from 'react';
+import AppLoading from 'expo-app-loading';
 const Stack = createStackNavigator<RootStackParamList>();
 
 function OnboadingRoute(): ReactElement {
@@ -30,10 +34,27 @@ function SwitchingStatus(status: UiContext.Status): ReactElement {
 
 export default function MainRoute(): ReactElement {
   const uiContext = React.useContext(UiContext.Context);
+  const [isReady, setReady] = useState(false);
+  const { setApplicationState } = React.useContext(Context);
+
+  const _cacheResourcesAsync = async () => {
+    const result = await retrieve();
+    if (result !== null) {
+      setApplicationState(Status.AUTHORIZED);
+    }
+    return result;
+  };
 
   return (
-    <NavigationContainer>
-      {SwitchingStatus(uiContext.applicationState)}
-    </NavigationContainer>
-  );
+    isReady ? (
+      <NavigationContainer>
+        {SwitchingStatus(uiContext.applicationState)}
+      </NavigationContainer>
+    ) : (
+      <AppLoading
+        startAsync={_cacheResourcesAsync}
+        onFinish={() => setReady(true)}
+        onError={console.warn}
+      />
+    ));
 }
