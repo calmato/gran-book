8d103abf11b1dd91e386d9984cadb723faad22d2
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-02-28 23:02:17 +0900

    test: ストアのテストヘルパーの更新

---
+++ web/admin/spec/helpers/response/admin.ts
@@ -0,0 +1,28 @@
+export const get = {
+  '/v1/admin': {
+    users: [
+      {
+        id: '00000000-0000-0000-00000000',
+        username: 'test-user',
+        email: 'test@calmato.com',
+        phoneNumber: '000-0000-0000',
+        role: 0,
+        thumbnailUrl: 'https://calmato.com/images/01',
+        selfIntroduction: 'よろしくお願いします',
+        lastName: 'テスト',
+        firstName: 'ユーザ',
+        lastNameKana: 'てすと',
+        firstNameKana: 'ゆーざ',
+        createdAt: '2021-01-01 00:00:00',
+        updatedAt: '2021-01-01 00:00:00',
+      },
+    ],
+    limit: 20,
+    offset: 0,
+    total: 1,
+    order: {
+      by: 'id',
+      direction: 'asc',
+    },
+  },
+}
