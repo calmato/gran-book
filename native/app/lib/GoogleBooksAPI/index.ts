c7f7480e2557008916839217f74803bf3f2aec29
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-03-01 11:11:34 +0900

    feat: lib配下にGoogle Books APIをコールする関数を配置する

---
+++ native/app/lib/GoogleBooksAPI/index.ts
@@ -0,0 +1,15 @@
+﻿import axios, { AxiosResponse } from 'axios';
+import { ISearchResponse } from '~/types/response/search';
+
+const url = 'https://www.googleapis.com/books/v1/volumes';
+
+export async function searchBook(param: string) {
+  return axios.get(`${url}?q=${param}`)
+    .then((res: AxiosResponse<ISearchResponse>) => {
+      return res.data;
+    })
+    .catch((err) => {
+      console.log('[debug: search]',err.response);
+      return null;
+    });
+}
