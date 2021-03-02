bec930801763eabd3b083bec3e0aca363bf4e0dc
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-03-02 14:52:25 +0900

    fix: Google Books APIの検索結果を40件取得にする

--- native/app/lib/GoogleBooksAPI/index.ts
+++ native/app/lib/GoogleBooksAPI/index.ts
@@ -3,8 +3,11 @@ import { ISearchResponse } from '~/types/response/search';
 
 const url = 'https://www.googleapis.com/books/v1/volumes';
 
+// TODO: スクロールでのページネーション対応
 export async function searchBook(param: string) {
-  return axios.get(`${url}?q=${param}`)
+  const index = 0;
+  const maxResults = 40;
+  return axios.get(`${url}?q=${param}&startIndex=${index}&maxResults=${maxResults}`)
     .then((res: AxiosResponse<ISearchResponse>) => {
       return res.data;
     })
