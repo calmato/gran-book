016cc5d96a245980c0886101d06e92232e852f5c
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-03-05 01:37:24 +0900

    feat: UserServiceの設定

--- api/server/user/internal/application/input/user.go
+++
@@ -1,6 +0,0 @@
-package input
-
-// GetUserProfile - ユーザプロフィールのリクエスト
-type GetUserProfile struct {
-	ID string `json:"id" validate:"required"`
-}
--- api/server/user/internal/application/validation/user.go
+++ api/server/user/internal/application/validation/user.go
@@ -1,15 +1,7 @@
 package validation
 
-import (
-	"github.com/calmato/gran-book/api/server/user/internal/application/input"
-	"github.com/calmato/gran-book/api/server/user/internal/domain/exception"
-	"golang.org/x/xerrors"
-)
-
 // UserRequestValidation - User関連のリクエストバリデータ
-type UserRequestValidation interface {
-	GetUserProfile(in *input.GetUserProfile) error
-}
+type UserRequestValidation interface{}
 
 type userRequestValidation struct {
 	validator RequestValidator
@@ -22,14 +14,4 @@ func NewUserRequestValidation() UserRequestValidation {
 	return &userRequestValidation{
 		validator: rv,
 	}
-}
-
-func (v *userRequestValidation) GetUserProfile(in *input.GetUserProfile) error {
-	ves := v.validator.Run(in)
-	if len(ves) == 0 {
-		return nil
-	}
-
-	err := xerrors.New("Failed to GetUserProfile for RequestValidation")
-	return exception.InvalidRequestValidation.New(err, ves...)
 }
