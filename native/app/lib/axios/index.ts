83a3316cf8fe667ab8440c73962faf4bb6f36392
濵田広大 <hamany0326@gmail.com>
2021-03-08 23:36:58 +0900

    fix: interanl/externalのapiように処理を分けた

--- native/app/lib/axios/index.ts
+++ native/app/lib/axios/index.ts
@@ -1,19 +1,2 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-import * as LocalStorage from '~/lib/local-storage';
-import { Auth } from '~/store/models';
-
-const instance: AxiosInstance = axios.create({
-  baseURL: process.env.API_URL,
-});
-
-instance.interceptors.request.use(async (config: AxiosRequestConfig) => {
-  const auth: Auth.Model = await LocalStorage.AuthStorage.retrieve();
-  if (auth) {
-    const token = `Bearer ${auth.token}`;
-    config.headers.Authorization = token;
-  }
-
-  return config;
-});
-
-export default instance;
+export {default as internal} from './internal'
+export {default as external} from './external'
