243bf64e37178664b4d69801c2fcd02e0adeba1b
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-03-11 03:02:16 +0900

    style: lint

--- api/server/book/internal/interface/gprc/v1/error.go
+++ api/server/book/internal/interface/gprc/v1/error.go
@@ -1,102 +1,93 @@
 package v1
 
-import (
-	"fmt"
+// func errorHandling(e error) error {
+// 	res := toGRPCError(e)
 
-	"github.com/calmato/gran-book/api/server/book/internal/domain/exception"
-	"google.golang.org/genproto/googleapis/rpc/errdetails"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-)
+// 	return res
+// }
 
-func errorHandling(e error) error {
-	res := toGRPCError(e)
+// func toGRPCError(e error) error {
+// 	c := getGRPCErrorCode(e)
 
-	return res
-}
+// 	st := status.New(c, c.String())
 
-func toGRPCError(e error) error {
-	c := getGRPCErrorCode(e)
+// 	switch c {
+// 	case codes.InvalidArgument, codes.AlreadyExists:
+// 		br := &errdetails.BadRequest{
+// 			FieldViolations: getValidationErrors(e),
+// 		}
 
-	st := status.New(c, c.String())
+// 		dt, err := st.WithDetails(br)
+// 		if err != nil {
+// 			dt = status.New(codes.Unknown, fmt.Sprintf("Unexpected error attaching metadata: %v", err))
+// 		}
 
-	switch c {
-	case codes.InvalidArgument, codes.AlreadyExists:
-		br := &errdetails.BadRequest{
-			FieldViolations: getValidationErrors(e),
-		}
+// 		return dt.Err()
+// 	case codes.Internal:
+// 		br := &errdetails.LocalizedMessage{
+// 			Locale:  "en-US",
+// 			Message: getError(e),
+// 		}
 
-		dt, err := st.WithDetails(br)
-		if err != nil {
-			dt = status.New(codes.Unknown, fmt.Sprintf("Unexpected error attaching metadata: %v", err))
-		}
+// 		dt, err := st.WithDetails(br)
+// 		if err != nil {
+// 			dt = status.New(codes.Unknown, fmt.Sprintf("Unexpected error attaching metadata: %v", err))
+// 		}
 
-		return dt.Err()
-	case codes.Internal:
-		br := &errdetails.LocalizedMessage{
-			Locale:  "en-US",
-			Message: getError(e),
-		}
+// 		return dt.Err()
+// 	default:
+// 		return st.Err()
+// 	}
+// }
 
-		dt, err := st.WithDetails(br)
-		if err != nil {
-			dt = status.New(codes.Unknown, fmt.Sprintf("Unexpected error attaching metadata: %v", err))
-		}
+// func getError(e error) string {
+// 	if err, ok := e.(exception.CustomError); ok {
+// 		return err.Error()
+// 	}
 
-		return dt.Err()
-	default:
-		return st.Err()
-	}
-}
+// 	return ""
+// }
 
-func getError(e error) string {
-	if err, ok := e.(exception.CustomError); ok {
-		return err.Error()
-	}
+// func getErrorCode(e error) exception.ErrorCode {
+// 	if err, ok := e.(exception.CustomError); ok {
+// 		return err.Code()
+// 	}
 
-	return ""
-}
+// 	return exception.Unknown
+// }
 
-func getErrorCode(e error) exception.ErrorCode {
-	if err, ok := e.(exception.CustomError); ok {
-		return err.Code()
-	}
+// func getGRPCErrorCode(e error) codes.Code {
+// 	switch getErrorCode(e) {
+// 	case exception.InvalidRequestValidation, exception.UnableConvertBase64:
+// 		return codes.InvalidArgument
+// 	case exception.NotFound:
+// 		return codes.NotFound
+// 	case exception.Conflict:
+// 		return codes.AlreadyExists
+// 	case exception.Forbidden:
+// 		return codes.PermissionDenied
+// 	case exception.ErrorInDatastore, exception.ErrorInStorage,
+// 		exception.ErrorInOtherAPI, exception.InvalidDomainValidation:
+// 		return codes.Internal
+// 	case exception.Unauthorized, exception.Expired:
+// 		return codes.Unauthenticated
+// 	default:
+// 		return codes.Unknown
+// 	}
+// }
 
-	return exception.Unknown
-}
+// func getValidationErrors(e error) []*errdetails.BadRequest_FieldViolation {
+// 	if err, ok := e.(exception.CustomError); ok {
+// 		ves := make([]*errdetails.BadRequest_FieldViolation, len(err.Validations()))
+// 		for i, ve := range err.Validations() {
+// 			ves[i] = &errdetails.BadRequest_FieldViolation{
+// 				Field:       ve.Field,
+// 				Description: ve.Message,
+// 			}
+// 		}
 
-func getGRPCErrorCode(e error) codes.Code {
-	switch getErrorCode(e) {
-	case exception.InvalidRequestValidation, exception.UnableConvertBase64:
-		return codes.InvalidArgument
-	case exception.NotFound:
-		return codes.NotFound
-	case exception.Conflict:
-		return codes.AlreadyExists
-	case exception.Forbidden:
-		return codes.PermissionDenied
-	case exception.ErrorInDatastore, exception.ErrorInStorage,
-		exception.ErrorInOtherAPI, exception.InvalidDomainValidation:
-		return codes.Internal
-	case exception.Unauthorized, exception.Expired:
-		return codes.Unauthenticated
-	default:
-		return codes.Unknown
-	}
-}
+// 		return ves
+// 	}
 
-func getValidationErrors(e error) []*errdetails.BadRequest_FieldViolation {
-	if err, ok := e.(exception.CustomError); ok {
-		ves := make([]*errdetails.BadRequest_FieldViolation, len(err.Validations()))
-		for i, ve := range err.Validations() {
-			ves[i] = &errdetails.BadRequest_FieldViolation{
-				Field:       ve.Field,
-				Description: ve.Message,
-			}
-		}
-
-		return ves
-	}
-
-	return []*errdetails.BadRequest_FieldViolation{}
-}
+// 	return []*errdetails.BadRequest_FieldViolation{}
+// }
