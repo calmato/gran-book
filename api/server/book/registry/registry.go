d2a172d3d4ba638176384e653d63f7b4e4084a21
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-03-11 22:38:43 +0900

    feat: 認証用の処理を実装

--- api/server/book/registry/registry.go
+++ api/server/book/registry/registry.go
@@ -1,14 +1,31 @@
 package registry
 
 import (
+	"github.com/calmato/gran-book/api/server/book/internal/application"
 	"github.com/calmato/gran-book/api/server/book/internal/infrastructure/repository"
+	"github.com/calmato/gran-book/api/server/book/internal/infrastructure/service"
 	gcs "github.com/calmato/gran-book/api/server/book/lib/firebase/storage"
 )
 
 // Registry - DIコンテナ
-type Registry struct{}
+type Registry struct {
+	AuthApplication application.AuthApplication
+}
 
 // NewRegistry - internalディレクトリ配下のファイルを読み込み
 func NewRegistry(db *repository.Client, s *gcs.Storage) *Registry {
-	return &Registry{}
+	auth := authInjection(db, s)
+
+	return &Registry{
+		AuthApplication: auth,
+	}
+}
+
+func authInjection(db *repository.Client, s *gcs.Storage) application.AuthApplication {
+	ar := repository.NewAuthRepository()
+	as := service.NewAuthService(ar)
+
+	aa := application.NewAuthApplication(as)
+
+	return aa
 }
