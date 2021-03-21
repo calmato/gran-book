7bed275da47841a60c293c92e6d4dc4f03715e5c
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-03-22 00:26:05 +0900

    feat: 書籍一括登録APIの実装

--- api/server/book/internal/interface/grpc/v1/book.go
+++ api/server/book/internal/interface/grpc/v1/book.go
@@ -63,6 +63,62 @@ func (s *BookServer) CreateBook(ctx context.Context, req *pb.CreateBookRequest) 
 	return res, nil
 }
 
+func (s *BookServer) CreateAndUpdateBooks(
+	ctx context.Context, req *pb.CreateAndUpdateBooksRequest,
+) (*pb.BookListResponse, error) {
+	_, err := s.AuthApplication.Authentication(ctx)
+	if err != nil {
+		return nil, errorHandling(err)
+	}
+
+	books := make([]*input.BookItem, len(req.GetItems()))
+	for i, item := range req.GetItems() {
+		as := make([]*input.BookAuthor, len(item.GetAuthors()))
+		for j, v := range item.GetAuthors() {
+			a := &input.BookAuthor{
+				Name: v.GetName(),
+			}
+
+			as[j] = a
+		}
+
+		cs := make([]*input.BookCategory, len(item.GetCategories()))
+		for j, v := range item.GetCategories() {
+			c := &input.BookCategory{
+				Name: v.GetName(),
+			}
+
+			cs[j] = c
+		}
+
+		bookItem := &input.BookItem{
+			Title:        item.GetTitle(),
+			Description:  item.GetDescription(),
+			Isbn:         item.GetIsbn(),
+			ThumbnailURL: item.GetThumbnailUrl(),
+			Version:      item.GetVersion(),
+			Publisher:    item.GetPublisher(),
+			PublishedOn:  item.GetPublishedOn(),
+			Authors:      as,
+			Categories:   cs,
+		}
+
+		books[i] = bookItem
+	}
+
+	in := &input.CreateAndUpdateBooks{
+		Books: books,
+	}
+
+	bs, err := s.BookApplication.MultipleCreateAndUpdate(ctx, in)
+	if err != nil {
+		return nil, errorHandling(err)
+	}
+
+	res := getBookListResponse(bs)
+	return res, nil
+}
+
 func getBookResponse(b *book.Book) *pb.BookResponse {
 	p := &pb.BookResponse_Publisher{}
 	if b.Publisher != nil {
@@ -103,5 +159,57 @@ func getBookResponse(b *book.Book) *pb.BookResponse {
 		Categories:   cs,
 		CreatedAt:    datetime.TimeToString(b.CreatedAt),
 		UpdatedAt:    datetime.TimeToString(b.UpdatedAt),
+	}
+}
+
+func getBookListResponse(bs []*book.Book) *pb.BookListResponse {
+	items := make([]*pb.BookListResponse_Item, len(bs))
+	for i, b := range bs {
+		p := &pb.BookListResponse_Publisher{}
+		if b.Publisher != nil {
+			p.Id = int64(b.Publisher.ID)
+			p.Name = b.Publisher.Name
+		}
+
+		as := make([]*pb.BookListResponse_Author, len(b.Authors))
+		for i, v := range b.Authors {
+			a := &pb.BookListResponse_Author{
+				Id:   int64(v.ID),
+				Name: v.Name,
+			}
+
+			as[i] = a
+		}
+
+		cs := make([]*pb.BookListResponse_Category, len(b.Categories))
+		for i, v := range b.Categories {
+			c := &pb.BookListResponse_Category{
+				Id:   int64(v.ID),
+				Name: v.Name,
+			}
+
+			cs[i] = c
+		}
+
+		item := &pb.BookListResponse_Item{
+			Id:           int64(b.ID),
+			Title:        b.Title,
+			Description:  b.Description,
+			Isbn:         b.Isbn,
+			ThumbnailUrl: b.ThumbnailURL,
+			Version:      b.Version,
+			PublishedOn:  datetime.DateToString(b.PublishedOn),
+			Publisher:    p,
+			Authors:      as,
+			Categories:   cs,
+			CreatedAt:    datetime.TimeToString(b.CreatedAt),
+			UpdatedAt:    datetime.TimeToString(b.UpdatedAt),
+		}
+
+		items[i] = item
+	}
+
+	return &pb.BookListResponse{
+		Items: items,
 	}
 }
