58e5536ed854fb1d1e43eba761791e189dc660c5
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-03-23 23:49:15 +0900

    refactor: エラーレスポンス値の修正

--- api/server/book/internal/application/input/book.go
+++ api/server/book/internal/application/input/book.go
@@ -2,15 +2,15 @@ package input
 
 // BookItem - 書籍登録/更新のリクエスト
 type BookItem struct {
-	Title        string          `json:"title" validate:"required,max=32"`
-	Description  string          `json:"description" validate:"max=1000"`
+	Title        string          `json:"title" validate:"required,max=64"`
+	Description  string          `json:"description" validate:"max=1600"`
 	Isbn         string          `json:"isbn" validate:"required"`
 	ThumbnailURL string          `json:"thumbnailUrl"`
 	Version      string          `json:"version"`
 	Publisher    string          `json:"publisher" validate:"max=32"`
 	PublishedOn  string          `json:"publishedOn"`
-	Authors      []*BookAuthor   `json:"authors" validate:"dive"`
-	Categories   []*BookCategory `json:"categories" validate:"dive"`
+	Authors      []*BookAuthor   `json:"authors" validate:"omitempty,dive"`
+	Categories   []*BookCategory `json:"categories" validate:"omitempty,dive"`
 }
 
 // CreateAndUpdateBooks - 書籍一括登録/更新のリクエスト
