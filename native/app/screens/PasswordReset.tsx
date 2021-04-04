b60bbdf04e29f3df9a5faf222c9106d6ff26e258
濵田広大 <hamany0326@gmail.com>
2021-04-04 10:56:40 +0900

    feat: Added navigation

--- native/app/screens/PasswordReset.tsx
+++ native/app/screens/PasswordReset.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, useMemo, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 import { AuthStackParamList } from '~/types/navigation';
 import { StackNavigationProp } from '@react-navigation/stack';
 import HeaderWithBackButton from '~/components/organisms/HeaderWithBackButton';
@@ -7,7 +7,8 @@ import MailInput from '~/components/molecules/MailInput';
 import { PasswordResetForm } from '~/types/forms';
 import { emailValidation } from '~/lib/validation';
 import { Button } from 'react-native-elements';
-import { authenticationAsync, sendPasswordResetEmail } from '~/store/usecases';
+import { sendPasswordResetEmail } from '~/store/usecases';
+import { generateErrorMessage } from '~/lib/util/ErrorUtil';
 
 const styles = StyleSheet.create({
   container: {
@@ -29,8 +30,20 @@ const PasswordReset = function PasswordReset(props: Props): ReactElement {
     email: '',
   });
 
+  const createAlertNotifyProfileEditError = (code: number) =>
+    Alert.alert('パスワード更新に失敗', `${generateErrorMessage(code)}`, [
+      {
+        text: 'OK',
+      },
+    ]);
+
   const resetRequest = async () => {
-    sendPasswordResetEmail(formData.email);
+    try {
+      sendPasswordResetEmail(formData.email);
+      navigation.navigate('SignUpCheckEmail', { email: formData.email });
+    } catch (err) {
+      createAlertNotifyProfileEditError(err.code);
+    }
   };
   const emailError: boolean = useMemo((): boolean => {
     return !emailValidation(formData.email);
@@ -49,11 +62,7 @@ const PasswordReset = function PasswordReset(props: Props): ReactElement {
         value={formData?.email}
         sameEmailError={false}
       />
-      <Button
-        disabled={hasError}
-        onPress={(resetRequest)}
-        title="メールを送信する"
-      />
+      <Button disabled={hasError} onPress={resetRequest} title="メールを送信する" />
     </View>
   );
 };
