ce3a100dc71d0caa9485f1ccacedee35a0f7c6bb
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-04-06 21:24:07 +0900

    test

--- api/gateway/native/src/index.ts
+++ api/gateway/native/src/index.ts
@@ -12,7 +12,7 @@ const app = express()
 const host: string = process.env.HOST || '0.0.0.0'
 const port: string = process.env.PORT || '3000'
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
 app.use(bodyParser.json())
 app.use(cors(corsOptions))
 app.use(accessLogHandler)
