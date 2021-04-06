1599ebd484846668bbfb92eb35c8f7ea6908e3c0
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-04-06 16:47:14 +0900

    style: 不要なimportを削除

--- native/app/lib/rakuten-books/index.ts
+++ native/app/lib/rakuten-books/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { IErrorResponse, ISearchResponse } from '~/types/response/external/rakuten-books';
 
 const baseUrl = 'https://app.rakuten.co.jp/services/api/BooksBook/Search';
