ad8e891db69070f4f853e57034c96853baa1d811
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-04-08 17:55:21 +0900

    feat: マイクロサービス間のInput/Output用型定義

--- api/gateway/native/src/types/input/book.ts
+++ api/gateway/native/src/types/input/book.ts
@@ -1,15 +1,56 @@
-export interface ICreateAndUpdateBooksInput {
-  items: Array<IBookItemInput>
+export interface IShowBookInput {
+  isbn: string
 }
 
-export interface IBookItemInput {
+export interface ICreateBookInput {
   title: string
+  titleKana: string
   description: string
   isbn: string
-  thumbnailURL: string
-  version: string
   publisher: string
   publishedOn: string
-  authors: Array<string>
-  categories: Array<string>
+  thumbnailUrl: string
+  rakutenUrl: string
+  rakutenGenreId: string
+  authors: Array<IBookAuthorInput>
+}
+
+export interface IUpdateBookInput {
+  title: string
+  titleKana: string
+  description: string
+  isbn: string
+  publisher: string
+  publishedOn: string
+  thumbnailUrl: string
+  rakutenUrl: string
+  rakutenGenreId: string
+  authors: Array<IBookAuthorInput>
+}
+
+export interface IBookAuthorInput {
+  name: string
+  nameKana: string
+}
+
+export interface IReadBookshelfInput {
+  bookId: number
+  impression: string
+  readOn: string
+}
+
+export interface IReadingBookshelfInput {
+  bookId: number
+}
+
+export interface IStackBookshelfInput {
+  bookId: number
+}
+
+export interface IWantBookshelfInput {
+  bookId: number
+}
+
+export interface IReleaseBookshelfInput {
+  bookId: number
 }
--- api/gateway/native/src/types/output/book.ts
+++ api/gateway/native/src/types/output/book.ts
@@ -1,18 +1,50 @@
 export interface IBookOutput {
   id: number
   title: string
+  titleKana: string
   description: string
   isbn: string
-  thumbnailUrl: string
-  version: string
   publisher: string
   publishedOn: string
-  authors: Array<string>
-  categories: Array<string>
+  thumbnailUrl: string
+  rakutenUrl: string
+  rakutenGenreId: string
+  createdAt: string
+  updatedAt: string
+  bookshelf: IBookBookshelfOutput
+  authors: Array<IBookAuthorOutput>
+  reviews: Array<IBookBookshelfOutput>
+}
+
+export interface IBookAuthorOutput {
+  name: string
+  nameKana: string
+}
+
+export interface IBookReviewOutput {
+  id: number
+  userId: string
+  score: number
+  impression: string
   createdAt: string
   updatedAt: string
 }
 
-export interface IBookListOutput {
-  books: Array<IBookOutput>
+export interface IBookBookshelfOutput {
+  id: number
+  status: number
+  readOn: string
+  createdAt: string
+  updatedAt: string
+}
+
+export interface IBookshelfOutput {
+  id: number
+  bookId: number
+  userId: string
+  status: number
+  impression: string
+  readOn: string
+  createdAt: string
+  updatedAt: string
 }
