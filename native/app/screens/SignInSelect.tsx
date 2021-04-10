3a7d73d47cab983b15c151188767df11340febd5
atsuhide <atsuhide.i@gmail.com>
2021-04-10 17:16:59 +0900

    feat: 画面遷移させるためにAuthとるところまで。500帰ってくるので中断

--- native/app/screens/SignInSelect.tsx
+++ native/app/screens/SignInSelect.tsx
@@ -1,6 +1,6 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import React, { ReactElement } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-elements';
 import { AuthStackParamList } from '~/types/navigation';
 import HeaderWithCloseButton from '~/components/organisms/HeaderWithCloseButton';
@@ -9,6 +9,10 @@ import TitleLogoText from '~/components/atoms/TitleLogoText';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
 import Firebase from 'firebase'
+import { useNavigation } from '@react-navigation/native';
+import { UiContext } from '~/lib/context';
+import { Status } from '~/lib/context/ui';
+import { generateErrorMessage } from '~/lib/util/ErrorUtil';
 
 type AuthSignInNavigationProp = StackNavigationProp<AuthStackParamList, 'SignUp'>;
 
@@ -27,13 +31,17 @@ const styles = StyleSheet.create({
 });
 
 interface Props {
-  navigation: AuthSignInNavigationProp;
+  actions: {
+    getAuth: () => Promise<void>;
+  };
 }
 
 WebBrowser.maybeCompleteAuthSession();
 
 const SignInSelect = function SignInSelect(props: Props): ReactElement {
-  const navigation = props.navigation;
+  const navigation = useNavigation();
+  const { setApplicationState } = React.useContext(UiContext);
+  const { getAuth } = props.actions;
 
   const [_request, response, promptAsync] = Google.useIdTokenAuthRequest(
     {
@@ -41,13 +49,26 @@ const SignInSelect = function SignInSelect(props: Props): ReactElement {
       },
   );
 
+  const createAlertNotifySignupError = (code: number) =>
+  Alert.alert('サインインに失敗', `${generateErrorMessage(code)}`, [
+    {
+      text: 'OK',
+    },
+  ]);
+
   const handleSignInWithGoogle = () => {
     promptAsync()
     .then(() => {
       console.log('loggedIn')
+      return getAuth();
     })
-    .catch((err:Error) => {
+    .then(() => {
+      console.log('gotAuth')
+      setApplicationState(Status.AUTHORIZED);
+    })
+    .catch((err) => {
       console.log(err)
+      createAlertNotifySignupError(err.code)
     })
   }
 
@@ -71,7 +92,7 @@ const SignInSelect = function SignInSelect(props: Props): ReactElement {
         </Text>
       </View>
       <SignInButtonGroup
-        handleSignInWithGoogle={() => promptAsync()}
+        handleSignInWithGoogle={() => handleSignInWithGoogle()}
         handleRegisterWithMail={() => navigation.navigate('SignUp')}
         handleSignInWithMail={() => navigation.navigate('SignIn')}
       />
