fa594d4bb918f8c75f3a952228b21540a2fe9837
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-04-12 02:15:26 +0900

    test: User DBの仕様変更に伴うテストコードの修正

--- api/server/user/internal/application/auth_test.go
+++ api/server/user/internal/application/auth_test.go
@@ -46,7 +46,6 @@ func TestAuthApplication_Authentication(t *testing.T) {
 					AddressLine1:     "貫井北町4-1-1",
 					AddressLine2:     "",
 					InstanceID:       "",
-					Activated:        true,
 					CreatedAt:        current,
 					UpdatedAt:        current,
 				},
@@ -105,12 +104,11 @@ func TestAuthApplication_Create(t *testing.T) {
 				Error error
 			}{
 				User: &user.User{
-					Username:  "test-user",
-					Email:     "test-user@calmato.com",
-					Password:  "12345678",
-					Gender:    0,
-					Role:      0,
-					Activated: true,
+					Username: "test-user",
+					Email:    "test-user@calmato.com",
+					Password: "12345678",
+					Gender:   0,
+					Role:     0,
 				},
 				Error: nil,
 			},
