c514cb4d54bc87d0ba37ba0c2827227acb46b0e5
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-04-14 02:12:24 +0900

    feat: Book APIへの接続設定

--- api/gateway/admin/src/plugins/grpc.ts
+++ api/gateway/admin/src/plugins/grpc.ts
@@ -1,4 +1,5 @@
 import { credentials } from '@grpc/grpc-js'
+import { BookServiceClient, IBookServiceClient } from '~/proto/book_apiv1_grpc_pb'
 import {
   AdminServiceClient,
   AuthServiceClient,
@@ -7,8 +8,10 @@ import {
 } from '~/proto/user_apiv1_grpc_pb'
 
 const userAPIURL: string = process.env.USER_API_URL || 'user_api:8080'
+const bookAPIURL: string = process.env.BOOK_API_URL || 'book_api:8080'
 
 const authClient: IAuthServiceClient = new AuthServiceClient(userAPIURL, credentials.createInsecure())
 const adminClient: IAdminServiceClient = new AdminServiceClient(userAPIURL, credentials.createInsecure())
+const bookClient: IBookServiceClient = new BookServiceClient(bookAPIURL, credentials.createInsecure())
 
-export { authClient, adminClient }
+export { authClient, adminClient, bookClient }
