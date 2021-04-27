49b6723246b0f03c0bd55a3bc07529e5b4d6a1da
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-04-28 02:21:15 +0900

    feat: 本棚の書籍一覧エンドポイントの型定義

--- api/gateway/native/src/types/response/book.ts
+++ api/gateway/native/src/types/response/book.ts
@@ -50,3 +50,37 @@ export interface IBookshelfResponse {
   createdAt: string
   updatedAt: string
 }
+
+export interface IBookshelfListResponse {
+  books: Array<IBookshelfListResponseBook>
+  limit: number
+  offset: number
+  total: number
+}
+
+export interface IBookshelfListResponseBook {
+  id: number
+  status: number
+  impression: string
+  readOn: string
+  createdAt: string
+  updatedAt: string
+  detail: IBookshelfListResponseDetail
+}
+
+export interface IBookshelfListResponseDetail {
+  id: number
+  title: string
+  titleKana: string
+  description: string
+  isbn: string
+  publisher: string
+  publishedOn: string
+  thumbnailUrl: string
+  rakutenUrl: string
+  rakutenGenreId: string
+  author: string
+  authorKana: string
+  createdAt: string
+  updatedAt: string
+}
