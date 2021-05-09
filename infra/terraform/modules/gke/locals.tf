41abce48052146a5ec70b44e5be4b67a2a2b50dc
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-05-09 16:06:15 +0900

    feat: GKE構築用のモジュールを編集

--- infra/terraform/modules/gke/locals.tf
+++ infra/terraform/modules/gke/locals.tf
@@ -1,5 +1,21 @@
 locals {
   gke_node_configs = {
-    for c in var.gke_node_configs : c.name => c
+    for c in var.gke_node_configs : c.name => {
+      name         = c.name
+      count        = c.count
+      preemptible  = c.preemptible
+      machine_type = c.machine_type
+      disk_type    = c.disk_type
+      disk_size_gb = c.disk_size_gb
+      oauth_scopes = flatten([
+        [
+          "https://www.googleapis.com/auth/cloud-platform",
+        ],
+        c.monitoring_enabled ? [
+          "https://www.googleapis.com/auth/logging.write",
+          "https://www.googleapis.com/auth/monitoring",
+        ] : [],
+      ])
+    }
   }
 }
