9c2fa59c6ebc40469fab8e5c87037f8e09041c45
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-05-18 23:05:19 +0900

    style: lint

--- web/admin/app/components/organisms/AdminEditContactForm.vue
+++ web/admin/app/components/organisms/AdminEditContactForm.vue
@@ -1,7 +1,11 @@
 <template>
   <v-form class="pa-4">
     <the-text-field v-model="form.params.email" :label="form.options.email.label" :rules="form.options.email.rules" />
-    <the-text-field v-model="form.params.phoneNumber" :label="form.options.phoneNumber.label" :rules="form.options.phoneNumber.rules" />
+    <the-text-field
+      v-model="form.params.phoneNumber"
+      :label="form.options.phoneNumber.label"
+      :rules="form.options.phoneNumber.rules"
+    />
     <v-btn color="primary" class="mt-4 mr-4" @click="onSubmit">変更する</v-btn>
     <v-btn class="mt-4" @click="onCancel">キャンセル</v-btn>
   </v-form>
