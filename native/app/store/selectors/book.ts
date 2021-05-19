d41b2a8ffe80ffea56a42f7a26ef1fed0db6aea6
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2021-05-19 23:27:14 +0900

    feat: filter関数を通してstateを参照するように変更

--- native/app/store/selectors/book.ts
+++ native/app/store/selectors/book.ts
@@ -1,6 +1,7 @@
 import { Book } from '~/store/models';
+import { ViewBooks } from '~/types/models/book';
 import { AppState } from '../modules';
 
-export const bookSelector = (state: AppState): Book.Model => {
-  return state.book;
+export const bookSelector = (state: AppState): ViewBooks => {
+  return Book.filterBooks(state.book);
 };
