13f70211ea477ca1c20c3600849e3ec2ad69ed0e
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-05-20 23:18:09 +0900

    feat: 書籍管理トップ画面のUI実装

---
+++ web/admin/app/pages/books/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <book-list />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import BookList from '~/components/templates/BookList.vue'
+
+export default defineComponent({
+  components: {
+    BookList,
+  },
+
+  setup() {},
+})
+</script>
