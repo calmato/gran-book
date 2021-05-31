0492ac606fb45ed481b1131f438ce4df5940bd6b
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2021-05-31 23:58:17 +0900

    feat: 楽天Books APIのページネーション機能の実装
    - useEffectの部分のcleanup処理について性能面で改善する余地あり
    - とりあえずリストの最後までスクロールしたら次のpageをAPIコールする

--- native/app/screens/SearchResult.tsx
+++ native/app/screens/SearchResult.tsx
@@ -1,11 +1,13 @@
 ﻿import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import React, { ReactElement, useCallback } from 'react';
+import React, { ReactElement, useCallback, useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import HeaderWithBackButton from '~/components/organisms/HeaderWithBackButton';
 import SearchResultItemList from '~/components/organisms/SearchResultItemList';
+import { searchBookByTitle } from '~/lib/rakuten-books';
 import { HomeTabStackPramList } from '~/types/navigation';
+import { ISearchResponse, ISearchResultItem } from '~/types/response/external/rakuten-books';
 import { COLOR } from '~~/constants/theme';
 
 const styles = StyleSheet.create({
@@ -22,8 +24,12 @@ interface Props {
 }
 
 const SearchResult = function SearchResult(props: Props): ReactElement {
-  const { keyword, results } = props.route.params;
+  const { keyword } = props.route.params;
   const navigation = props.navigation;
+
+  const [result, setResult] = useState<ISearchResponse>();
+  const [books, setBooks] = useState<ISearchResultItem[]>([]);
+  const [page, setPage] = useState<number>(1);
 
   const selectBook = useCallback(
     (item) => {
@@ -32,12 +38,34 @@ const SearchResult = function SearchResult(props: Props): ReactElement {
     [navigation],
   );
 
+  // TODO: パフォーマンス面で要リファクタ
+  useEffect(() => {
+    let unmounted = false;
+    const f = async () => {
+      const res = await searchBookByTitle(keyword, page);
+      if (!unmounted && page === 1) {
+        setResult(res.data);
+        setBooks(res.data.Items);
+      } else if (result && page <= result?.pageCount) {
+        setBooks([...books, ...res.data.Items]);
+      }
+    };
+    f();
+    return () => {
+      unmounted = true;
+    };
+  }, [page]);
+
   return (
     <View>
       <HeaderWithBackButton onPress={() => navigation.goBack()} title={keyword} />
-      <Text style={styles.totalItemsTextStyle}>検索結果：{results.count}件</Text>
-      <ScrollView style={{ marginBottom: 'auto' }}>
-        <SearchResultItemList results={results} onPress={selectBook} />
+      <Text style={styles.totalItemsTextStyle}>検索結果：{result?.count}件</Text>
+      <ScrollView
+        style={{ marginBottom: 'auto' }}
+        onMomentumScrollEnd={() => {
+          setPage(page + 1);
+        }}>
+        <SearchResultItemList resultItems={books} onPress={selectBook} />
       </ScrollView>
     </View>
   );
