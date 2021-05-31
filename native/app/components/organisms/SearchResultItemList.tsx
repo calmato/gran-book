986f99cb434e8b5473dd185e6d776a71cfe0fd77
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2021-05-31 23:57:53 +0900

    fix: propsの型定義を変更

--- native/app/components/organisms/SearchResultItemList.tsx
+++ native/app/components/organisms/SearchResultItemList.tsx
@@ -1,21 +1,21 @@
 ﻿import React, { ReactElement } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { ISearchResponse, ISearchResultItem } from '~/types/response/external/rakuten-books';
+import { ISearchResultItem } from '~/types/response/external/rakuten-books';
 import SearchResultItem from '~/components/molecules/SearchResultItem';
 
 const styles = StyleSheet.create({});
 
 interface Props {
-  results: ISearchResponse;
+  resultItems: ISearchResultItem[];
   onPress: (item: Partial<ISearchResultItem>) => void;
 }
 
 const SearchResultItemList = function SearchResultItemList(props: Props): ReactElement {
-  const results = props.results;
+  const items = props.resultItems;
 
   return (
     <View>
-      {results.Items.map((item) => (
+      {items.map((item) => (
         <SearchResultItem key={item.isbn} book={item} onPress={() => props.onPress(item)} />
       ))}
     </View>
