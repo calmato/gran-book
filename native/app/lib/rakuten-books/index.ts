fc72a3a9e67a71872ad708310c62813e4ca72640
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2021-06-01 21:55:46 +0900

    style: 関数コメントを追加

--- native/app/lib/rakuten-books/index.ts
+++ native/app/lib/rakuten-books/index.ts
@@ -10,6 +10,12 @@ const hits = 30;
 
 const applicationId = process.env.RAKUTEN_BOOKS_APPLICATION_ID;
 
+/**
+ * 楽天 Book APIを利用して書籍を検索する関数
+ * @param title 検索したい書籍のタイトル
+ * @param page ページ番号（任意、デフォルト値は1）
+ * @returns Promise<AxiosResponse<ISearchResponse>>
+ */
 export async function searchBookByTitle(title: string, page = 1) {
   const url = `${baseUrl}/${version}?format=${format}&title=${encodeURI(
     title,
