4cff2da2e910967405957e1040305bca3c38e268
kaito <sea_rr10_ch@outlook.com>
2021-06-02 01:40:11 +0900

    いらないもの削除

--- native/app/screens/Messages.tsx
+++ native/app/screens/Messages.tsx
@@ -1,52 +1,49 @@
-import React, { useState, useMemo, useEffect, useCallback, } from 'react';
-import { View, StyleSheet, SafeAreaView, TextInput, Text, KeyboardAvoidingView, FlatList, } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import firebase from '~/lib/firebase';
 import { getMessageDocRef } from '~/store/usecases/auth';
 import { COLOR } from '~~/constants/theme';
-import { MaterialIcons, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
-import { MessageForm, TransferMessageForm } from '~/types/forms';
+import { MaterialIcons, Ionicons } from '@expo/vector-icons';
+import { TransferMessageForm } from '~/types/forms';
 import { Header } from 'react-native-elements';
 import { GiftedChat, Send } from 'react-native-gifted-chat';
 
 export const MessagesScreen = () => {
-  const [textData, setText] = useState ({
-    text: '',
-    createdAt: firebase.firestore.Timestamp.now(),
-    _id: '',
-  });
   const [messages, setMessages] = useState<TransferMessageForm[]>([]);
   const docRef = getMessageDocRef();
-
-  const onSend = (newMessage: TransferMessageForm[]) => {
-    messages.forEach(async (newmessage) => {
-      (await docRef).add(newmessage);});
-    setMessages((messages) => GiftedChat.append(messages, newMessage));
+  let initialStatus = true;
+  const onSend = (newMessages: TransferMessageForm[]) => {
+    newMessages.forEach(async (newMessage) => {
+      (await docRef).add(newMessage);
+    });
   };
-  const getMessage = async () => {
-    const messages = [] as TransferMessageForm[];
-    await firebase.firestore().collection('messages').orderBy('createdAt', 'desc').limit(20)
-      .onSnapshot((snapshot) => {
+  const getMessage =  () => {
+    firebase.firestore().collection('messages').orderBy('createdAt', 'desc').limit(30)
+      .onSnapshot( (snapshot) => {
         snapshot.docChanges().forEach((change) => {
-          if (change.type === 'added') {
+          if (change?.type === 'added'){
             const messageInfo: TransferMessageForm = {
               _id: change.doc.data()._id,
               createdAt: change.doc.data().createdAt.toDate(),
               text: change.doc.data().text,
               user: change.doc.data().user,
             };
-            setMessages((messages)=>[...messages, messageInfo]);
-          }
+            if (initialStatus){
+              setMessages((messages)=>[...messages, messageInfo]);
+            } else {
+              setMessages((messages)=> [messageInfo, ...messages]);
+            }}
         });
+        initialStatus = false;
       });
   };
   useEffect(() => {
     getMessage();
-  }, []);
+  });
 
   return (
     <View style={styles.container}>
-
       <ExpoStatusBar style="light" />
       <Header
         leftComponent={
@@ -82,7 +79,6 @@ export const MessagesScreen = () => {
           name: 'Kaito'
         }}
       />
-
     </View>
   );
 };
@@ -99,12 +95,6 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'flex-end',
   },
-  messagesContainer: {
-    marginBottom: 'auto',
-    width: 'auto',
-    padding: 10
-  },
-
   sendContainer: {
     justifyContent: 'center',
     alignItems: 'flex-end',
@@ -114,45 +104,10 @@ const styles = StyleSheet.create({
     marginRight: '3%',
     bottom: 0,
   },
-
-  inputText: {
-    color: COLOR.TEXT_DEFAULT,
-    borderWidth: 1,
-    borderColor: COLOR.BACKGROUND_GREY,
-    backgroundColor: COLOR.BACKGROUND_GREY,
-    height: 'auto',
-    minHeight: '20%',
-    width: '70%',
-    marginVertical: '5%',
-    marginHorizontal: '5%',
-    borderRadius: 10,
-    paddingHorizontal: 10,
-  },
-  inputImage: {
-    marginLeft: '3%',
-    marginBottom: '5%',
-    color: COLOR.TEXT_GRAY
-  },
   sendButton: {
     marginRight: '3%',
     marginBottom: '5%',
     color: COLOR.PRIMARY
-  },
-  sendButtonDisabled: {
-    marginRight: '3%',
-    marginBottom: '5%',
-    color: COLOR.LIGHT_GREY
-  },
-  chatFooter: {
-    backgroundColor: COLOR.BACKGROUND_WHITE,
-    height: 'auto',
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-    bottom: 0,
-    minHeight: '10%',
-    maxHeight: '20%'
   },
 });
 
