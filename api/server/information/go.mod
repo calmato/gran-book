1f6fbe321c9119423e7ee2fe803c1758641d5beb
濵田広大 <hamany0326@gmail.com>
2021-06-02 18:21:54 +0900

    Refactor: lint fix

--- api/server/information/go.mod
+++ api/server/information/go.mod
@@ -12,13 +12,17 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
 	github.com/prometheus/client_golang v1.9.0
 	go.uber.org/zap v1.16.0
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 	google.golang.org/api v0.46.0
 	google.golang.org/genproto v0.0.0-20210429181445-86c259c2b4ab
 	google.golang.org/grpc v1.37.0
 	google.golang.org/protobuf v1.26.0
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gorm.io/driver/mysql v1.1.0
 	gorm.io/gorm v1.21.10
 )
--- api/server/information/internal/domain/notification/entity.go
+++ api/server/information/internal/domain/notification/entity.go
@@ -4,21 +4,21 @@ import "time"
 
 // Notification - お知らせエンティティ
 type Notification struct {
-	ID					int			`gorm:primaryKey;autoIncrement;<-:create"`
-	AuthorID		string
-	EditorID		string
-	CategoryID	string
-	Title				string
-	Description	string
-	Importance	string
-	CreatedAt		time.Time
-	UpdatedAt		time.Time
+	ID          int `gorm:primaryKey;autoIncrement;<-:create"`
+	AuthorID    string
+	EditorID    string
+	CategoryID  string
+	Title       string
+	Description string
+	Importance  string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
 
 // Category	-	カテゴリーエンティティ
 type Category struct {
-	ID					int	`gorm:primaryKey;autoIncrement;<-:create"`
-	Name				string
-	CreatedAt		time.Time
-	UpdatedAt		time.Time
+	ID        int `gorm:primaryKey;autoIncrement;<-:create"`
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
