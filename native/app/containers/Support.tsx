9988a5bf6a7e613638e1ccafd1d5ff3041f51acb
atsuhide <atsuhide.i@gmail.com>
2021-06-03 21:08:34 +0900

    fix: お問い合わせ入力画面でメアドを入力させるように変更。既に登録されている場合はローカルのAuthに保存されているメアドをデフォルト表示。ついでにボタン実装しました。

--- native/app/containers/Support.tsx
+++ native/app/containers/Support.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Support from '~/screens/Support';
+import { Auth } from '~/store/models';
+import { authSelector } from '~/store/selectors';
 
 export default function ConnectedSupport(): JSX.Element {
-  return <Support />;
+  const auth: Auth.Model = useSelector(authSelector);
+  return <Support auth={auth} />;
 }
--- native/app/screens/Support.tsx
+++ native/app/screens/Support.tsx
@@ -1,11 +1,14 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useMemo, useState } from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
-import { ListItem, Text, Input } from 'react-native-elements';
+import { ListItem, Text, Input, Button } from 'react-native-elements';
 import { MaterialIcons } from '@expo/vector-icons';
 import { COLOR } from '~~/constants/theme';
 import { useNavigation } from '@react-navigation/native';
 import HeaderWithBackButton from '~/components/organisms/HeaderWithBackButton';
 import { Picker } from '@react-native-picker/picker';
+import MailInput from '~/components/molecules/MailInput';
+import { emailValidation } from '~/lib/validation';
+import { Auth } from '~/store/models';
 
 const styles = StyleSheet.create({
   container: {
@@ -40,8 +43,13 @@ const styles = StyleSheet.create({
   },
 });
 
-const Support = function Support(): ReactElement {
+interface Props {
+  auth: Auth.Model;
+}
+
+const Support = function Support(props: Props): ReactElement {
   const navigation = useNavigation();
+  const { auth } = props;
   const supportSubjectList = [
     '（お問い合わせの内容を選択）',
     '本人情報を変更できない',
@@ -53,48 +61,86 @@ const Support = function Support(): ReactElement {
   ];
   const [supportSubject, setsupportSubject] = useState(supportSubjectList[0]);
   const [detail, setdetail] = useState('');
+  const [emailForm, setState] = useState(auth.email);
+
+  const emailError: boolean = useMemo((): boolean => {
+    return !emailValidation(emailForm);
+  }, [emailForm]);
+
+  const formError: boolean = useMemo((): boolean => {
+    return !detail;
+  }, [detail]);
+
+  const canSubmit = useMemo((): boolean => {
+    return !(emailError || formError);
+  }, [emailError, formError]);
+
+  const generateErrorMessage = () => {
+    if (formError) {
+      return 'お問い合わせ内容を入力してください';
+    } else {
+      return undefined;
+    }
+  };
+
+  const handleSubmit = undefined;
 
   return (
-    <View>
-      <ScrollView stickyHeaderIndices={[0]}>
-        <HeaderWithBackButton title="お問い合わせ" onPress={() => navigation.goBack()} />
-        <Text style={styles.subtilte}>アカウント設定</Text>
-        <Text style={styles.defaultText}>お問い合わせ前に、ヘルプをご確認ください。</Text>
-        <ListItem bottomDivider onPress={() => undefined}>
-          <ListItem.Content>
-            <ListItem.Title>{'ヘルプページへ'}</ListItem.Title>
-          </ListItem.Content>
-          <MaterialIcons name="keyboard-arrow-right" size={24} color="black" />
-        </ListItem>
-        <Text style={styles.subtilte}>お問い合わせ</Text>
-        <Text style={[{ color: COLOR.TEXT_WARNING }, styles.defaultText]}>
-          取引中の商品についてのお問い合わせは、取引画面下の「この取引についてのお問い合わせ」よりおねがいいたします。
-        </Text>
-        <Text style={styles.defaultText}>サポート返信目安：4営業日</Text>
-        <Picker
-          selectedValue={supportSubject}
-          onValueChange={(itemVlue, _itemIndex) => {
-            setsupportSubject(itemVlue);
-          }}>
-          {supportSubjectList.map((item) => {
-            return <Picker.Item label={item} value={item} key={item} />;
-          })}
-        </Picker>
-        {supportSubject != supportSubjectList[0] && (
+    <ScrollView stickyHeaderIndices={[0]}>
+      <HeaderWithBackButton title="お問い合わせ" onPress={() => navigation.goBack()} />
+      <Text style={styles.subtilte}>アカウント設定</Text>
+      <Text style={styles.defaultText}>お問い合わせ前に、ヘルプをご確認ください。</Text>
+      <ListItem bottomDivider onPress={() => undefined}>
+        <ListItem.Content>
+          <ListItem.Title>{'ヘルプページへ'}</ListItem.Title>
+        </ListItem.Content>
+        <MaterialIcons name="keyboard-arrow-right" size={24} color="black" />
+      </ListItem>
+      <Text style={styles.subtilte}>お問い合わせ</Text>
+      <Text style={[{ color: COLOR.TEXT_WARNING }, styles.defaultText]}>
+        取引中の商品についてのお問い合わせは、取引画面下の「この取引についてのお問い合わせ」よりおねがいいたします。
+      </Text>
+      <Text style={styles.defaultText}>サポート返信目安：4営業日</Text>
+      <Picker
+        selectedValue={supportSubject}
+        onValueChange={(itemVlue, _itemIndex) => {
+          setsupportSubject(itemVlue);
+        }}>
+        {supportSubjectList.map((item) => {
+          return <Picker.Item label={item} value={item} key={item} />;
+        })}
+      </Picker>
+      {supportSubject != supportSubjectList[0] && (
+        <View>
+          <Text style={styles.defaultText}>返答を受信したいメールアドレス</Text>
+          <MailInput
+            onChangeText={(text) => setState(text)}
+            value={emailForm}
+            hasError={emailError}
+            sameEmailError={false}
+          />
+          <Text style={styles.defaultText}>お問い合わせ内容</Text>
           <Input
             style={styles.questionDetail}
             placeholder={'お問い合わせ内容を入力してください'}
             multiline={true}
             onChangeText={(text) => setdetail(text)}
             value={detail}
+            errorMessage={generateErrorMessage()}
           />
-        )}
-        <Text style={styles.defaultText}>
-          {'※問題のある行為や商品、コメントについては、商品詳細下部にある通報機能をご利用ください。\n\n' +
-            '※サポート返信目安を過ぎても返信がない場合は、お手数ですが再度お問い合わせください。'}
-        </Text>
-      </ScrollView>
-    </View>
+        </View>
+      )}
+      <Text style={styles.defaultText}>
+        {'※問題のある行為や商品、コメントについては、商品詳細下部にある通報機能をご利用ください。\n\n' +
+          '※サポート返信目安を過ぎても返信がない場合は、お手数ですが再度お問い合わせください。'}
+      </Text>
+      <Button
+        style={{ alignSelf: 'center' }}
+        disabled={!canSubmit}
+        onPress={handleSubmit}
+        title="お問い合わせを送信"
+      />
+    </ScrollView>
   );
 };
 
