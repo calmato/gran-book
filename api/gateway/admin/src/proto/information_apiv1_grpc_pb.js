56ffcde8f8b8412b07a9cf834a441c8f79a7d605
atsuhide <atsuhide.i@gmail.com>
2021-06-05 00:46:27 +0900

    feat:　gRPCのルール定義するファイル生成

--- api/gateway/admin/src/proto/information_apiv1_grpc_pb.js
+++ api/gateway/admin/src/proto/information_apiv1_grpc_pb.js
@@ -4,6 +4,17 @@
 var grpc = require('@grpc/grpc-js');
 var proto_information_apiv1_pb = require('../proto/information_apiv1_pb.js');
 
+function serialize_proto_CreateInquiryRequest(arg) {
+  if (!(arg instanceof proto_information_apiv1_pb.CreateInquiryRequest)) {
+    throw new Error('Expected argument of type proto.CreateInquiryRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_proto_CreateInquiryRequest(buffer_arg) {
+  return proto_information_apiv1_pb.CreateInquiryRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
 function serialize_proto_HelloRequest(arg) {
   if (!(arg instanceof proto_information_apiv1_pb.HelloRequest)) {
     throw new Error('Expected argument of type proto.HelloRequest');
@@ -26,6 +37,17 @@ function deserialize_proto_HelloResponse(buffer_arg) {
   return proto_information_apiv1_pb.HelloResponse.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
+function serialize_proto_InquiryResponse(arg) {
+  if (!(arg instanceof proto_information_apiv1_pb.InquiryResponse)) {
+    throw new Error('Expected argument of type proto.InquiryResponse');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_proto_InquiryResponse(buffer_arg) {
+  return proto_information_apiv1_pb.InquiryResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
 
 var NotificationServiceService = exports.NotificationServiceService = {
   reply: {
@@ -42,3 +64,18 @@ var NotificationServiceService = exports.NotificationServiceService = {
 };
 
 exports.NotificationServiceClient = grpc.makeGenericClientConstructor(NotificationServiceService);
+var InquiryServiceService = exports.InquiryServiceService = {
+  createInquiry: {
+    path: '/proto.InquiryService/CreateInquiry',
+    requestStream: false,
+    responseStream: false,
+    requestType: proto_information_apiv1_pb.CreateInquiryRequest,
+    responseType: proto_information_apiv1_pb.InquiryResponse,
+    requestSerialize: serialize_proto_CreateInquiryRequest,
+    requestDeserialize: deserialize_proto_CreateInquiryRequest,
+    responseSerialize: serialize_proto_InquiryResponse,
+    responseDeserialize: deserialize_proto_InquiryResponse,
+  },
+};
+
+exports.InquiryServiceClient = grpc.makeGenericClientConstructor(InquiryServiceService);
--- api/gateway/native/src/proto/information_apiv1_grpc_pb.d.ts
+++ api/gateway/native/src/proto/information_apiv1_grpc_pb.d.ts
@@ -40,3 +40,36 @@ export class NotificationServiceClient extends grpc.Client implements INotificat
     public reply(request: proto_information_apiv1_pb.HelloRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_information_apiv1_pb.HelloResponse) => void): grpc.ClientUnaryCall;
     public reply(request: proto_information_apiv1_pb.HelloRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_information_apiv1_pb.HelloResponse) => void): grpc.ClientUnaryCall;
 }
+
+interface IInquiryServiceService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
+    createInquiry: IInquiryServiceService_ICreateInquiry;
+}
+
+interface IInquiryServiceService_ICreateInquiry extends grpc.MethodDefinition<proto_information_apiv1_pb.CreateInquiryRequest, proto_information_apiv1_pb.InquiryResponse> {
+    path: "/proto.InquiryService/CreateInquiry";
+    requestStream: false;
+    responseStream: false;
+    requestSerialize: grpc.serialize<proto_information_apiv1_pb.CreateInquiryRequest>;
+    requestDeserialize: grpc.deserialize<proto_information_apiv1_pb.CreateInquiryRequest>;
+    responseSerialize: grpc.serialize<proto_information_apiv1_pb.InquiryResponse>;
+    responseDeserialize: grpc.deserialize<proto_information_apiv1_pb.InquiryResponse>;
+}
+
+export const InquiryServiceService: IInquiryServiceService;
+
+export interface IInquiryServiceServer extends grpc.UntypedServiceImplementation {
+    createInquiry: grpc.handleUnaryCall<proto_information_apiv1_pb.CreateInquiryRequest, proto_information_apiv1_pb.InquiryResponse>;
+}
+
+export interface IInquiryServiceClient {
+    createInquiry(request: proto_information_apiv1_pb.CreateInquiryRequest, callback: (error: grpc.ServiceError | null, response: proto_information_apiv1_pb.InquiryResponse) => void): grpc.ClientUnaryCall;
+    createInquiry(request: proto_information_apiv1_pb.CreateInquiryRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_information_apiv1_pb.InquiryResponse) => void): grpc.ClientUnaryCall;
+    createInquiry(request: proto_information_apiv1_pb.CreateInquiryRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_information_apiv1_pb.InquiryResponse) => void): grpc.ClientUnaryCall;
+}
+
+export class InquiryServiceClient extends grpc.Client implements IInquiryServiceClient {
+    constructor(address: string, credentials: grpc.ChannelCredentials, options?: Partial<grpc.ClientOptions>);
+    public createInquiry(request: proto_information_apiv1_pb.CreateInquiryRequest, callback: (error: grpc.ServiceError | null, response: proto_information_apiv1_pb.InquiryResponse) => void): grpc.ClientUnaryCall;
+    public createInquiry(request: proto_information_apiv1_pb.CreateInquiryRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_information_apiv1_pb.InquiryResponse) => void): grpc.ClientUnaryCall;
+    public createInquiry(request: proto_information_apiv1_pb.CreateInquiryRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_information_apiv1_pb.InquiryResponse) => void): grpc.ClientUnaryCall;
+}
