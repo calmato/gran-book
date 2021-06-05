6e6ea51af56e80b82d55d300087be262ba5a27e4
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-06-05 19:19:32 +0900

    style: lint

--- api/server/user/internal/infrastructure/repository/context.go
+++ api/server/user/internal/infrastructure/repository/context.go
@@ -1,16 +1,14 @@
 package repository
 
-import "strings"
-
 const (
-	chatRoomCollection    = "rooms"
-	chatMessageCollection = "messages"
+	chatRoomCollection = "rooms"
+	// chatMessageCollection = "messages"
 )
 
 func getChatRoomCollection() string {
 	return chatRoomCollection
 }
 
-func getChatMessageCollection(roomID string) string {
-	return strings.Join([]string{chatRoomCollection, roomID, chatMessageCollection}, "/")
-}
+// func getChatMessageCollection(roomID string) string {
+// 	return strings.Join([]string{chatRoomCollection, roomID, chatMessageCollection}, "/")
+// }
