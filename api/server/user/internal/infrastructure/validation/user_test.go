61df70e3335d2410f79d7545b30bddee138dd471
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-06-06 23:52:59 +0900

    refactor: DomainValidationのテストをリファクタ

--- api/server/user/internal/infrastructure/validation/user_test.go
+++ api/server/user/internal/infrastructure/validation/user_test.go
@@ -12,40 +12,45 @@ import (
 )
 
 func TestUserDomainValidation_User(t *testing.T) {
-	current := time.Now()
+	type args struct {
+		user *user.User
+	}
 
+	current := time.Now().Local()
 	testCases := map[string]struct {
-		User     *user.User
-		Expected error
+		args args
+		want error
 	}{
 		"ok": {
-			User: &user.User{
-				ID:               "00000000-0000-0000-0000-000000000000",
-				Username:         "test-user",
-				Gender:           0,
-				Email:            "test-user@calmato.com",
-				PhoneNumber:      "000-0000-0000",
-				Role:             0,
-				ThumbnailURL:     "",
-				SelfIntroduction: "",
-				LastName:         "テスト",
-				FirstName:        "ユーザ",
-				LastNameKana:     "てすと",
-				FirstNameKana:    "ゆーざ",
-				PostalCode:       "000-0000",
-				Prefecture:       "東京都",
-				City:             "小金井市",
-				AddressLine1:     "貫井北町4-1-1",
-				AddressLine2:     "",
-				InstanceID:       "",
-				CreatedAt:        current,
-				UpdatedAt:        current,
+			args: args{
+				user: &user.User{
+					ID:               "00000000-0000-0000-0000-000000000000",
+					Username:         "test-user",
+					Gender:           0,
+					Email:            "test-user@calmato.com",
+					PhoneNumber:      "000-0000-0000",
+					Role:             0,
+					ThumbnailURL:     "",
+					SelfIntroduction: "",
+					LastName:         "テスト",
+					FirstName:        "ユーザ",
+					LastNameKana:     "てすと",
+					FirstNameKana:    "ゆーざ",
+					PostalCode:       "000-0000",
+					Prefecture:       "東京都",
+					City:             "小金井市",
+					AddressLine1:     "貫井北町4-1-1",
+					AddressLine2:     "",
+					InstanceID:       "",
+					CreatedAt:        current,
+					UpdatedAt:        current,
+				},
 			},
-			Expected: nil,
+			want: nil,
 		},
 	}
 
-	for result, tc := range testCases {
+	for name, tc := range testCases {
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 
@@ -53,39 +58,44 @@ func TestUserDomainValidation_User(t *testing.T) {
 		defer ctrl.Finish()
 
 		urm := mock_user.NewMockRepository(ctrl)
-		urm.EXPECT().GetUIDByEmail(ctx, tc.User.Email).Return(tc.User.ID, nil)
+		urm.EXPECT().GetUIDByEmail(ctx, tc.args.user.Email).Return(tc.args.user.ID, nil)
 
-		t.Run(result, func(t *testing.T) {
+		t.Run(name, func(t *testing.T) {
 			target := NewUserDomainValidation(urm)
 
-			got := target.User(ctx, tc.User)
-			if !reflect.DeepEqual(got, tc.Expected) {
-				t.Fatalf("want %#v, but %#v", tc.Expected, got)
+			got := target.User(ctx, tc.args.user)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("want %#v, but %#v", tc.want, got)
 			}
 		})
 	}
 }
 
 func TestUserDomainValidation_Relationship(t *testing.T) {
-	current := time.Now()
+	type args struct {
+		relationship *user.Relationship
+	}
 
+	current := time.Now().Local()
 	testCases := map[string]struct {
-		Relationship *user.Relationship
-		Expected     error
+		args args
+		want error
 	}{
 		"ok": {
-			Relationship: &user.Relationship{
-				ID:         1,
-				FollowID:   "00000000-0000-0000-0000-000000000000",
-				FollowerID: "11111111-1111-1111-1111-111111111111",
-				CreatedAt:  current,
-				UpdatedAt:  current,
+			args: args{
+				relationship: &user.Relationship{
+					ID:         1,
+					FollowID:   "00000000-0000-0000-0000-000000000000",
+					FollowerID: "11111111-1111-1111-1111-111111111111",
+					CreatedAt:  current,
+					UpdatedAt:  current,
+				},
 			},
-			Expected: nil,
+			want: nil,
 		},
 	}
 
-	for result, tc := range testCases {
+	for name, tc := range testCases {
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 
@@ -94,15 +104,15 @@ func TestUserDomainValidation_Relationship(t *testing.T) {
 
 		urm := mock_user.NewMockRepository(ctrl)
 		urm.EXPECT().
-			GetRelationshipIDByUID(ctx, tc.Relationship.FollowID, tc.Relationship.FollowerID).
-			Return(tc.Relationship.ID, nil)
+			GetRelationshipIDByUID(ctx, tc.args.relationship.FollowID, tc.args.relationship.FollowerID).
+			Return(tc.args.relationship.ID, nil)
 
-		t.Run(result, func(t *testing.T) {
+		t.Run(name, func(t *testing.T) {
 			target := NewUserDomainValidation(urm)
 
-			got := target.Relationship(ctx, tc.Relationship)
-			if !reflect.DeepEqual(got, tc.Expected) {
-				t.Fatalf("want %#v, but %#v", tc.Expected, got)
+			got := target.Relationship(ctx, tc.args.relationship)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("want %#v, but %#v", tc.want, got)
 			}
 		})
 	}
