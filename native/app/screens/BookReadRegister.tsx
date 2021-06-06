52f6e6e13d19eb02abcb5c62e76047b4a912aa3e
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2021-06-06 21:42:10 +0900

    feat: 登録ボタンを押した後の遷移を記述しておく

--- native/app/screens/BookReadRegister.tsx
+++ native/app/screens/BookReadRegister.tsx
@@ -1,8 +1,8 @@
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useCallback, useState } from 'react';
 import { StyleSheet, View, ScrollView, Text } from 'react-native';
-import { Input, Button } from 'react-native-elements';
+import { Button, Input } from 'react-native-elements';
 import BookNameAuthorRegister from '~/components/organisms/BookNameAuthorRegister';
 import HeaderWithBackButton from '~/components/organisms/HeaderWithBackButton';
 import ReadDate from '~/components/organisms/ReadDate';
@@ -27,6 +27,10 @@ const BookReadRegister = function BookReadRegister(props: Props): ReactElement {
     impresstion: '',
     isDateUnknown: false,
   });
+
+  const handleRegisterButtonClick = useCallback(() => {
+    props.navigation.goBack();
+  }, [props.navigation]);
 
   return (
     <View style={styles.container}>
@@ -67,7 +71,7 @@ const BookReadRegister = function BookReadRegister(props: Props): ReactElement {
           multiline={true}
         />
         <View style={{ alignItems: 'center', marginBottom: 20 }}>
-          <Button onPress={undefined} title="本を登録する" />
+          <Button onPress={handleRegisterButtonClick} title="本を登録する" />
         </View>
       </ScrollView>
     </View>
