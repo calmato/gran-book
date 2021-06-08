c39963d1974725d00bdf0d298cfad9277cb972a4
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-06-09 02:51:29 +0900

    feat: ChatRepository - 一覧取得メソッドの追加

--- api/server/user/internal/domain/chat/repository.go
+++ api/server/user/internal/domain/chat/repository.go
@@ -1,8 +1,13 @@
 package chat
 
-import "context"
+import (
+	"context"
+
+	"github.com/calmato/gran-book/api/server/user/internal/domain"
+)
 
 // Repository - Chatレポジトリ
 type Repository interface {
+	ListRoom(ctx context.Context, q *domain.ListQuery, uid string) ([]*Room, error)
 	CreateRoom(ctx context.Context, cr *Room) error
 }
