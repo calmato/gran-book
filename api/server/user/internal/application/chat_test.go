37de05d160d72e930ceb58fd2d5c0b41f2a2531e
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-06-09 16:13:02 +0900

    test: ChatApplicationのテスト追加

--- api/server/user/internal/application/chat_test.go
+++ api/server/user/internal/application/chat_test.go
@@ -4,13 +4,83 @@ import (
 	"context"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/calmato/gran-book/api/server/user/internal/application/input"
+	"github.com/calmato/gran-book/api/server/user/internal/domain"
 	"github.com/calmato/gran-book/api/server/user/internal/domain/chat"
 	mock_validation "github.com/calmato/gran-book/api/server/user/mock/application/validation"
 	mock_chat "github.com/calmato/gran-book/api/server/user/mock/domain/chat"
 	"github.com/golang/mock/gomock"
 )
+
+func TestChatApplication_ListRoom(t *testing.T) {
+	type args struct {
+		uid string
+	}
+	type want struct {
+		rooms []*chat.Room
+		err   error
+	}
+
+	current := time.Now().Local()
+	testCases := map[string]struct {
+		args args
+		want want
+	}{
+		"ok": {
+			args: args{
+				uid: "00000000-0000-0000-0000-000000000000",
+			},
+			want: want{
+				rooms: []*chat.Room{
+					{
+						ID:        "11111111-1111-1111-1111-111111111111",
+						UserIDs:   []string{"00000000-0000-0000-0000-000000000000"},
+						CreatedAt: current,
+						UpdatedAt: current,
+					},
+				},
+				err: nil,
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		q := &domain.ListQuery{
+			Order: &domain.QueryOrder{
+				By:        "updatedAt",
+				Direction: "desc",
+			},
+		}
+
+		crvm := mock_validation.NewMockChatRequestValidation(ctrl)
+
+		csm := mock_chat.NewMockService(ctrl)
+		csm.EXPECT().ListRoom(ctx, q, tc.args.uid).Return(tc.want.rooms, tc.want.err)
+
+		t.Run(name, func(t *testing.T) {
+			target := NewChatApplication(crvm, csm)
+
+			rooms, err := target.ListRoom(ctx, tc.args.uid)
+			if !reflect.DeepEqual(err, tc.want.err) {
+				t.Fatalf("want %#v, but %#v", tc.want.err, err)
+				return
+			}
+
+			if !reflect.DeepEqual(rooms, tc.want.rooms) {
+				t.Fatalf("want %#v, but %#v", tc.want.rooms, rooms)
+				return
+			}
+		})
+	}
+}
 
 func TestChatApplication_CreateRoom(t *testing.T) {
 	type args struct {
