7374b4aabe5eee1e994da1a9c8eaa938646262ec
atsuhide <atsuhide.i@gmail.com>
2021-06-15 21:04:29 +0900

    feat: インフラ層の実装

---
+++ api/server/information/internal/infrastructure/repository/inquiry.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/calmato/gran-book/api/server/information/internal/domain/exception"
+	"github.com/calmato/gran-book/api/server/information/internal/domain/inquiry"
+	"gorm.io/gorm/clause"
+)
+
+type inquiryRepository struct {
+	client *Client
+}
+
+//NewInquiryRepository - InquiryRepositoryの生成
+func NewInquiryRepository(c *Client) inquiry.Repository {
+	return &inquiryRepository{
+		client: c,
+	}
+}
+
+func (r *inquiryRepository) Create(ctx context.Context, b *inquiry.Inquiry) error {
+	tx := r.client.db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		return err
+	}
+
+	err := tx.Omit(clause.Associations).Create(&b).Error
+	if err != nil {
+		tx.Rollback()
+		return exception.ErrorInDatastore.New(err)
+	}
+
+	return tx.Commit().Error
+}
