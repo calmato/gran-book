0953eacf28c9158cccdf67d1b259b28ca8bd601e
kaito <sea_rr10_ch@outlook.com>
2021-06-17 21:36:59 +0900

    fix: チェック用修正

--- native/app/screens/Messages.tsx
+++ native/app/screens/Messages.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
@@ -9,22 +8,6 @@ import { MaterialIcons, Ionicons } from '@expo/vector-icons';
 import { TransferMessageForm } from '~/types/forms';
 import { Header } from 'react-native-elements';
 import { GiftedChat, Send } from 'react-native-gifted-chat';
-import { MaterialIcons, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React, { useState, useMemo, useEffect } from 'react';
-import {
-  View,
-  StyleSheet,
-  TextInput,
-  FlatList,
-} from 'react-native';
-import { Header } from 'react-native-elements';
-import { ScrollView } from 'react-native-gesture-handler';
-import { MessageItem } from '~/components/organisms/MessageItem';
-import firebase from '~/lib/firebase';
-import { getMessageDocRef } from '~/store/usecases/auth';
-import { MessageForm } from '~/types/forms';
-import { COLOR } from '~~/constants/theme';
 
 export const MessagesScreen = () => {
   const [messages, setMessages] = useState<TransferMessageForm[]>([]);
@@ -35,22 +18,27 @@ export const MessagesScreen = () => {
       (await docRef).add(newMessage);
     });
   };
-  const getMessage =  () => {
-    firebase.firestore().collection('messages').orderBy('createdAt', 'desc').limit(30)
-      .onSnapshot( (snapshot) => {
+  const getMessage = () => {
+    firebase
+      .firestore()
+      .collection('messages')
+      .orderBy('createdAt', 'desc')
+      .limit(30)
+      .onSnapshot((snapshot) => {
         snapshot.docChanges().forEach((change) => {
-          if (change?.type === 'added'){
+          if (change?.type === 'added') {
             const messageInfo: TransferMessageForm = {
               _id: change.doc.data()._id,
               createdAt: change.doc.data().createdAt.toDate(),
               text: change.doc.data().text,
               user: change.doc.data().user,
             };
-            if (initialStatus){
-              setMessages((messages)=>[...messages, messageInfo]);
+            if (initialStatus) {
+              setMessages((messages) => [...messages, messageInfo]);
             } else {
-              setMessages((messages)=> [messageInfo, ...messages]);
-            }}
+              setMessages((messages) => [messageInfo, ...messages]);
+            }
+          }
         });
         initialStatus = false;
       });
@@ -63,37 +51,28 @@ export const MessagesScreen = () => {
     <View style={styles.container}>
       <ExpoStatusBar style="light" />
       <Header
-        leftComponent={
-          <MaterialIcons
-            name="keyboard-arrow-left"
-            size={24}
-          />
-        }
+        leftComponent={<MaterialIcons name="keyboard-arrow-left" size={24} />}
         centerComponent={{
           text: 'Name',
-          style: styles.header
+          style: styles.header,
         }}
         centerContainerStyle={{
-          height: 40
+          height: 40,
         }}
       />
       <GiftedChat
-        messages= {messages}
-        renderSend= {(props) => {
+        messages={messages}
+        renderSend={(props) => {
           return (
             <Send {...props} containerStyle={styles.sendContainer}>
-              <Ionicons
-                style={styles.sendButton}
-                name= "send"
-                size= {32}
-              />
+              <Ionicons style={styles.sendButton} name="send" size={32} />
             </Send>
           );
         }}
-        onSend={messages => onSend(messages)}
+        onSend={(messages) => onSend(messages)}
         user={{
           _id: 1,
-          name: 'Kaito'
+          name: 'Kaito',
         }}
       />
     </View>
@@ -106,7 +85,7 @@ const styles = StyleSheet.create({
     backgroundColor: COLOR.BACKGROUND_GREY,
   },
   header: {
-    color:COLOR.TEXT_TITLE,
+    color: COLOR.TEXT_TITLE,
     fontSize: 20,
     fontWeight: 'bold',
     flexDirection: 'column',
@@ -124,7 +103,7 @@ const styles = StyleSheet.create({
   sendButton: {
     marginRight: '3%',
     marginBottom: '5%',
-    color: COLOR.PRIMARY
+    color: COLOR.PRIMARY,
   },
 });
 
