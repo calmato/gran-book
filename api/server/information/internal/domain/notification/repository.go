b3eccc4c817ba330c80a021820cfaf7c9558c918
濵田広大 <hamany0326@gmail.com>
2021-06-18 23:01:24 +0900

    Feat: ブランチ変えたいからここまでのをcommit

--- api/server/information/internal/domain/notification/repository.go
+++ api/server/information/internal/domain/notification/repository.go
@@ -2,15 +2,9 @@ package notification
 
 import (
 	"context"
-
-	"github.com/calmato/gran-book/api/server/information/internal/domain"
 )
 
 // Repository - Notificationリポジトリ
 type Repository interface {
-	ListNotification(ctx context.Context, q *domain.ListQuery) ([]*Notification, error)
-	ShowNotication(ctx context.Context, notificatinID int) (*Notification, error)
 	CreateNotification(ctx context.Context, n *Notification) error
-	UpdateNotification(ctx context.Context, n *Notification) error
-	DeleteNotification(ctx context.Context, notificatinID int) error
 }
