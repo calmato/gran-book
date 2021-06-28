119170376af8d97ed849d59365ad576ce2a4d6ea
taba2424 <tadashi-112@hotmail.co.jp>
2021-06-28 10:34:18 +0900

    feat: チャットルームAPIの型定義

--- api/gateway/native/src/types/input/index.ts
+++ api/gateway/native/src/types/input/index.ts
@@ -1,3 +1,4 @@
 export * from './auth'
 export * from './book'
+export * from './chat'
 export * from './user'
---
+++ api/gateway/native/src/types/output/chat.ts
@@ -0,0 +1,23 @@
+export interface IChatRoomOutput {
+  id: string
+  userIds: Array<string>
+  createdAt: string
+  updatedAt: string
+}
+
+export interface IChatRoomListOutput {
+  rooms: Array<IChatRoomListOutputRoom>
+}
+
+export interface IChatRoomListOutputMessage {
+  userId: string
+  text: string
+  image: string
+  createdAt: string
+}
+
+export interface IChatRoomListOutputRoom {
+  id: string
+  userIds: Array<string>
+  latestMessage: IChatRoomListOutputMessage
+}
