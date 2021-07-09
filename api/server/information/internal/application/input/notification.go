964df424abae36b118dfcc032142593f5e06dade
濵田広大 <hamany0326@gmail.com>
2021-07-10 03:28:20 +0900

    Feat: Added other notification input

--- api/server/information/internal/application/input/notification.go
+++ api/server/information/internal/application/input/notification.go
@@ -1,8 +1,25 @@
 package input
 
-// Notification - お知らせ登録/更新のリクエスト
+// Notification - お知らせ登録のリクエスト
 type CreateNotification struct {
 	Title       string `json:"title" validate:"required,max=64"`
 	Description string `json:"description" validate:"required,max=2000"`
 	Importance  string `json:"importance" validate:"required,max=64"`
+}
+
+// Notification - お知らせの更新のリクエスト
+type UpdaeteNotification struct {
+	Title       string `json:"title" validate:"required,max=64"`
+	Description string `json:"description" validate:"required,max=2000"`
+	Importance  string `json:"importance" validate:"required,max=64"`
+}
+
+// Notifcation - お知らせの取得のリクエスト
+type GetNotification struct {
+	ID int `json:"id" validate:"required"`
+}
+
+// Notification - お知らせの削除のリクエスト
+type DeleteNotification struct {
+	ID int `json:"id" validate:"required"`
 }
