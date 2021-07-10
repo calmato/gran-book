301e2089a6eb37064220b4d49bf434555ceac785
taba2424 <tadashi-112@hotmail.co.jp>
2021-07-10 10:58:25 +0900

    feat: add dependency injection for inquiry service

--- api/server/information/registry/registry.go
+++ api/server/information/registry/registry.go
@@ -13,16 +13,19 @@ import (
 type Registry struct {
 	AuthApplication         application.AuthApplication
 	NotificationApplication application.NotificationApplication
+	InquiryApplication      application.InquiryApplication
 }
 
 // NewRegistry - internalディレクトリ配下のファイルを読み込み
 func NewRegistry(db *repository.Client, s *gcs.Storage) *Registry {
 	auth := authInjection()
 	notification := notificationInjection(db)
+	inquiry := InquiryInjection(db)
 
 	return &Registry{
 		AuthApplication:         auth,
 		NotificationApplication: notification,
+		InquiryApplication:      inquiry,
 	}
 }
 
@@ -44,4 +47,14 @@ func notificationInjection(db *repository.Client) application.NotificationApplic
 	na := application.NewNotificationApplication(nrv, ns)
 
 	return na
+}
+
+func InquiryInjection(db *repository.Client) application.InquiryApplication {
+	ir := repository.NewInquiryRepository(db)
+	is := service.NewInquiryService(ir)
+
+	irv := rv.NewInquiryRequestValidation()
+	ia := application.NewInquiryApplication(irv, is)
+
+	return ia
 }
