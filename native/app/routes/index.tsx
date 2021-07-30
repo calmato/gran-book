d79537105365a1629818eb3dd20a160b8a87f4d6
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2021-07-30 23:20:36 +0900

    refactor: Onboadingのルーティングを別に切り出す

--- native/app/routes/index.tsx
+++ native/app/routes/index.tsx
@@ -1,23 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import React, { ReactElement } from 'react';
 import { usePrepare } from '~/hooks/usePrepare';
 import * as UiContext from '~/lib/context/ui';
-import AuthRoute from '~/routes/AuthRoute';
+import OnboadingRoute from '~/routes/OnboadingRoute';
 import ServiceRoute from '~/routes/ServiceRoute';
-import Onboarding from '~/screens/Onboarding';
 import SplashScreen from '~/screens/SplashScreen';
-import { RootStackParamList } from '~/types/navigation';
-const Stack = createStackNavigator<RootStackParamList>();
-
-function OnboadingRoute(): ReactElement {
-  return (
-    <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Onboarding" component={Onboarding} />
-      <Stack.Screen name="SignInSelect" component={AuthRoute} />
-    </Stack.Navigator>
-  );
-}
 
 // アプリ起動時、認証情報によってページ遷移
 function SwitchingStatus(status: UiContext.Status): ReactElement {
