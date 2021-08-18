4396bce92add1ba78d971af374a9fe55afdf8425
kaito <sea_rr10_ch@outlook.com>
2021-08-19 01:18:55 +0900

    feat: 感想一覧画面のUI調整

--- native/app/screens/OwnReviews.tsx
+++ native/app/screens/OwnReviews.tsx
@@ -1,14 +1,44 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import React, { ReactElement, useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { Header, ListItem, Avatar } from 'react-native-elements';
+import { Badge } from 'react-native-elements/dist/badge/Badge';
+import { ScrollView } from 'react-native-gesture-handler';
 import HeaderText from '~/components/atoms/HeaderText';
 import { Auth } from '~/store/models';
 import { getOwnReviews } from '~/store/usecases';
 import { IReviewResponse } from '~/types/response';
-import { COLOR } from '~~/constants/theme';
+import { COLOR, FONT_SIZE } from '~~/constants/theme';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  badgeStyle: {
+    backgroundColor: COLOR.BACKGROUND_WHITE,
+    alignSelf: 'flex-start',
+    marginStart: 10,
+    marginTop: 10,
+    height: 30,
+    width: 150,
+    borderRadius: 75,
+  },
+  reviewListStyle: {
+    marginTop: 8,
+    height: '100%',
+  },
+  reviewStyle: {
+    flexDirection: 'column',
+    alignSelf: 'stretch',
+  },
+  titleStyle: {
+    fontSize: FONT_SIZE.LISTITEM_TITLE,
+    fontWeight: 'bold',
+    paddingBottom: 4,
+    marginBottom: 4,
+  },
+  subtitleStyle: {
+    fontSize: FONT_SIZE.LISTITEM_SUBTITLE,
+    paddingVertical: 4,
+  },
+});
 
 interface Props {
   auth: Auth.Model;
@@ -34,37 +64,46 @@ const nullData: IReviewResponse = {
 };
 
 export const OwnReviews = function OwnReviews(props: Props): ReactElement {
-  const [reviews, setReviews] = useState<IReviewResponse>(nullData);
+  const [reviewList, setReviewList] = useState<IReviewResponse>(nullData);
 
   useEffect(() => {
     const f = async () => {
       const res = await getOwnReviews(props.auth.id);
-      setReviews(res.data);
+      setReviewList(res.data);
     };
     f();
   }, []);
 
-  const review = reviews.reviews;
+  const review = reviewList.reviews;
   return (
     <View>
       <Header centerComponent={<HeaderText title="自分の感想" />} />
-      {review.map((review) => (
-        <View key={review.id}>
-          <ListItem key={review.id}>
-            {review.book.thumbnailUrl !== '' ? (
-              <Avatar source={{ uri: review.book.thumbnailUrl }} />
-            ) : (
-              <Avatar>
-                <MaterialIcons name="person-outline" size={36} color={COLOR.GREY} />
-              </Avatar>
-            )}
-            <ListItem.Content>
-              <ListItem.Title>{review.book.title}</ListItem.Title>
-              <ListItem.Subtitle>{review.impression}</ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-        </View>
-      ))}
+      <Badge
+        value={<Text style={{ fontSize: 16 }}>{`${reviewList.total}件`}</Text>}
+        badgeStyle={styles.badgeStyle}
+      />
+      <ScrollView style={styles.reviewListStyle}>
+        {review.map((review) => (
+          <View key={review.id}>
+            <ListItem key={review.id} bottomDivider={true} pad={20}>
+              {review.book.thumbnailUrl !== '' ? (
+                <Avatar source={{ uri: review.book.thumbnailUrl }} size="large" />
+              ) : (
+                <Avatar>
+                  <MaterialIcons name="person-outline" size={36} color={COLOR.GREY} />
+                </Avatar>
+              )}
+              <ListItem.Content style={styles.reviewStyle}>
+                <ListItem.Title style={styles.titleStyle}>{review.book.title}</ListItem.Title>
+                <ListItem.Subtitle style={styles.subtitleStyle}>
+                  {review.impression}
+                </ListItem.Subtitle>
+              </ListItem.Content>
+              <ListItem.Chevron name="create" size={32} />
+            </ListItem>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };
