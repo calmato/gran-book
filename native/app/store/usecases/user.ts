77089930c73c83495be5b3f34f93ccf19da6b3b6
atsuhide <atsuhide.i@gmail.com>
2021-08-28 18:18:29 +0900

    fix: 他人のプロフィールをAPIで取得する関数を修正

--- native/app/store/usecases/user.ts
+++ native/app/store/usecases/user.ts
@@ -59,53 +59,12 @@ export function getOwnProfileAsync(id: string) {
 }
 
 export function getOtherProfileAsync(id: string) {
-  return async (dispatch: Dispatch): Promise<void> => {
-    return await internal
-      .get(`/v1/users/${id}/profile`)
-      .then(async (res: AxiosResponse<UserProfileV1Response.AsObject>) => {
-        const {
-          id,
-          username,
-          thumbnailUrl,
-          selfIntroduction,
-          isFollow,
-          isFollower,
-          followCount,
-          followerCount,
-          reviewCount,
-          rating,
-          productsList,
-        } = res.data;
-
-        const products: User.product[] = productsList.map((val: UserProfileV1Response.Product.AsObject) => {
-          const product: User.product = {
-            id: val.id,
-            name: val.name,
-            thumbnailUrl: val.thumbnailUrl,
-            authors: val.authorsList,
-          };
-
-          return product;
-        });
-
-        const values: User.UserValues = {
-          id,
-          username,
-          thumbnailUrl,
-          selfIntroduction,
-          isFollow,
-          isFollower,
-          followCount,
-          followerCount,
-          reviewCount,
-          rating,
-          products,
-        };
-
-        dispatch(setUser(values));
-      })
-      .catch((err: Error) => {
-        throw err;
-      });
-  };
-}
+  return internal
+    .get(`/v1/users/${id}/profile`)
+    .then(async (res: AxiosResponse<UserProfileV1Response.AsObject>) => {
+      return res
+    })
+    .catch((err: Error) => {
+      throw err;
+    });
+};
