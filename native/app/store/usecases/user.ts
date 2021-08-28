933faab538ed7d5a0e193279a60fe707f6f61a0d
atsuhide <atsuhide.i@gmail.com>
2021-08-28 17:58:07 +0900

    feat:  OtherProfileをAPIから取得する関数を定義

--- native/app/store/usecases/user.ts
+++ native/app/store/usecases/user.ts
@@ -7,7 +7,6 @@ import { UserProfileV1Response } from '~/types/api/user_apiv1_response_pb';
 
 export function getOwnProfileAsync(id: string) {
   return async (dispatch: Dispatch): Promise<void> => {
-    console.log('http', 'getOwnProfile');
     return await internal
       .get(`/v1/users/${id}/profile`)
       .then(async (res: AxiosResponse<UserProfileV1Response.AsObject>) => {
@@ -54,6 +53,58 @@ export function getOwnProfileAsync(id: string) {
       })
       .catch((err: Error) => {
         console.log(err);
+        throw err;
+      });
+  };
+}
+
+export function getOtherProfileAsync(id: string) {
+  return async (dispatch: Dispatch): Promise<void> => {
+    return await internal
+      .get(`/v1/users/${id}/profile`)
+      .then(async (res: AxiosResponse<UserProfileV1Response.AsObject>) => {
+        const {
+          id,
+          username,
+          thumbnailUrl,
+          selfIntroduction,
+          isFollow,
+          isFollower,
+          followCount,
+          followerCount,
+          reviewCount,
+          rating,
+          productsList,
+        } = res.data;
+
+        const products: User.product[] = productsList.map((val: UserProfileV1Response.Product.AsObject) => {
+          const product: User.product = {
+            id: val.id,
+            name: val.name,
+            thumbnailUrl: val.thumbnailUrl,
+            authors: val.authorsList,
+          };
+
+          return product;
+        });
+
+        const values: User.UserValues = {
+          id,
+          username,
+          thumbnailUrl,
+          selfIntroduction,
+          isFollow,
+          isFollower,
+          followCount,
+          followerCount,
+          reviewCount,
+          rating,
+          products,
+        };
+
+        dispatch(setUser(values));
+      })
+      .catch((err: Error) => {
         throw err;
       });
   };
