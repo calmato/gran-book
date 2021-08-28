6121ede0bc81f31c2fc52100ee1372e998b37222
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-08-28 18:41:44 +0900

    feat: setup jest.config.js

--- native/jest.config.js
+++ native/jest.config.js
@@ -1,8 +1,32 @@
 module.exports = {
   preset: 'jest-expo',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['dotenv/config'],
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/app/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/tmp/**',
+  ],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/tmp/',
+    '<rootDir>/app/types/api/',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'ts',
+    'tsx',
+  ],
+  moduleDirectories: [
+    'node_modules',
+  ],
+  setupFiles: [
+    'dotenv/config',
+  ],
   testMatch: [
-    '**/*.test.ts?(x)'
-  ]
+    '**/__tests__/**/*.(js|ts)?(x)',
+    '**/?(*.)(spec|test).(js|ts)?(x)'
+  ],
 };
