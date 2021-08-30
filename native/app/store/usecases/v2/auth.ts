85068d01357ee09d65266b9d9dad687efc05d794
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2021-08-31 08:45:33 +0900

    fix: code styleの調整

--- native/app/store/usecases/v2/auth.ts
+++ native/app/store/usecases/v2/auth.ts
@@ -3,9 +3,12 @@ import firebase from '~/lib/firebase';
 /**
  * firebase authenticationを使って email と password でログインを実行します。
  * @param payload
- * @returns { Promise<firebase.auth.UserCredential.user | null | undefined> }
+ * @returns { Promise<firebase.auth.UserCredential.user | null > }
  */
-export async function signInWithEmailAndPassword(payload: { email: string; password: string }) {
+export async function signInWithEmailAndPassword(payload: {
+  email: string;
+  password: string;
+}): Promise<firebase.User | null> {
   const { email, password } = payload;
 
   try {
